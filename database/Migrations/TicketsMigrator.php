57adeb981102021c100ce3b213ff71fa7d97bc70
Jewel <cep.jewel@gmail.com>
2021-03-03 00:31:57 +0600

    added last customer timing

--- database/Migrations/TicketsMigrator.php
+++ database/Migrations/TicketsMigrator.php
@@ -17,10 +17,10 @@ class TicketsMigrator
         if ($wpdb->get_var("SHOW TABLES LIKE '$table'") != $table) {
             $sql = "CREATE TABLE $table (
                 `id` BIGINT(20) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                `customer_id` BIGINT(20) UNSIGNED NOT NULL,
-                `agent_id` BIGINT(20) UNSIGNED NOT NULL,
-                `category_id` BIGINT(20) UNSIGNED NOT NULL,
-                `product_id` BIGINT(20) UNSIGNED NOT NULL,
+                `customer_id` BIGINT(20) UNSIGNED NULL,
+                `agent_id` BIGINT(20) UNSIGNED NULL,
+                `category_id` BIGINT(20) UNSIGNED NULL,
+                `product_id` BIGINT(20) UNSIGNED NULL,
                 `product_source` VARCHAR(192) NULL,
                 `privacy` VARCHAR(100) DEFAULT 'private',
                 `priority` VARCHAR(100) DEFAULT 'normal',
