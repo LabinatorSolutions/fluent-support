9948efb7fbc62679407b51a4b6d1411224d40930
Jewel <cep.jewel@gmail.com>
2021-03-03 21:18:07 +0600

    Create ByMailHandler.php

---
+++ app/Services/MailerInbox/ByMailHandler.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace FluentSupport\App\Services\MailerInbox;
+
+use FluentSupport\App\Models\Customer;
+use FluentSupport\App\Models\Response;
+use FluentSupport\App\Models\Ticket;
+use FluentSupport\Framework\Support\Arr;
+
+class ByMailHandler
+{
+    public static function handleEmailData($data)
+    {
+        $subject = self::getActualSubject(Arr::get($data, 'subject', 'Subject not defined'));
+
+        $onBehalf = Arr::get($data, 'sender');
+        $fullName = $onBehalf['name'];
+        unset($onBehalf['name']);
+
+        $nameArray = explode(' ', $fullName);
+        if (count($nameArray) >= 2) {
+            $onBehalf['last_name'] = array_pop($nameArray);
+            $onBehalf['last_name'] = implode(' ', $nameArray);
+        } else if ($fullName) {
+            $onBehalf['first_name'] = $fullName;
+        }
+
+        $customer = Customer::getCustomerFromData($onBehalf);
+        if (!$customer) {
+            $customer = Customer::maybeCreateCustomer($onBehalf);
+        }
+
+        $existingTicket = Ticket::where('customer_id', $customer->id)
+            ->where('title', 'like', '%%' . $subject . '%%')
+            ->first();
+
+        $responseOrTicketData = [
+            'title'   => $subject,
+            'content' => wp_unslash(wp_kses_post($data['content']))
+        ];
+
+        if (!$existingTicket) {
+            $responseOrTicketData['customer_id'] = $customer->id;
+            $data['source'] = 'email';
+
+            $data = apply_filters('fluent_support/create_ticket_data', $data, $customer);
+            do_action('fluent_support/before_ticket_create', $data, $customer);
+
+            $createdTicket = Ticket::create($data);
+
+            do_action('fluent_support/ticket_created', $createdTicket, $customer);
+            return [
+                'type'      => 'new_ticket',
+                'ticket_id' => $createdTicket->id
+            ];
+        }
+
+        // we have to create a response
+        $responseOrTicketData['person_id'] = $customer->id;
+        $responseOrTicketData['ticket_id'] = $existingTicket->id;
+        $responseOrTicketData['conversation_type'] = 'response';
+        $responseOrTicketData['source'] = 'mail';
+        $createdResponse = Response::create($responseOrTicketData);
+        if ($existingTicket->status != 'active') {
+            $existingTicket->status = 'active';
+            $existingTicket->last_customer_response = current_time('mysql');
+            $existingTicket->response_count += 1;
+            $existingTicket->save();
+        }
+        do_action('fluent_support/response_added_by_customer', $createdResponse, $existingTicket, $customer);
+        return [
+            'type'        => 'new_response',
+            'ticket_id'   => $existingTicket->id,
+            'response_id' => $createdResponse->id
+        ];
+    }
+
+
+    private static function getActualSubject($string)
+    {
+        $prefix = 'Re: ';
+        if (substr($string, 0, strlen($prefix)) == $prefix) {
+            $string = substr($string, strlen($string));
+        }
+        return $string;
+    }
+}
