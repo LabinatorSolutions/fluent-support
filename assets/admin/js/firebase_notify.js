3647a05e152c8d4e0e5191043d5067fbe060b2cc
Rafi Ahmed <66135964+rafiahmedd@users.noreply.github.com>
2021-10-25 11:40:31 +0600

    Search option added in support staffs

--- assets/admin/js/firebase_notify.js
+++ assets/admin/js/firebase_notify.js
@@ -1,1 +1,10 @@
-console.log("TicketActivityService");
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************************************!*\
+  !*** ./resources/admin/firebase_notify.js ***!
+  \********************************************/
+// import TicketActivityService from './Bits/TicketActivityService';
+console.log('TicketActivityService');
+/******/ })()
+;
+//# sourceMappingURL=firebase_notify.js.map
