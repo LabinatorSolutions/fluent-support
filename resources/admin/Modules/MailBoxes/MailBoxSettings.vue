6364bf7da0fb3e20a3206695e00b8b10f819f6bd
Rafi Ahmed <66135964+rafiahmedd@users.noreply.github.com>
2021-10-27 18:34:51 +0600

    Added Translation to Mailboxes

--- resources/admin/Modules/MailBoxes/MailBoxSettings.vue
+++ resources/admin/Modules/MailBoxes/MailBoxSettings.vue
@@ -1,33 +1,33 @@
 <template>
     <div class="fs_mailbox_settings">
         <el-form :data="mailbox" label-position="top">
-            <el-form-item label="Inbox Name">
+            <el-form-item :label="$t('Inbox Name')">
                 <el-input type="text" v-model="mailbox.name"></el-input>
             </el-form-item>
-            <el-form-item label="Business Email">
+            <el-form-item :label="$t('Business Email')">
                 <el-input type="email" v-model="mailbox.email"></el-input>
-                <p>Please make sure your website can send emails from this email address</p>
+                <p>{{$t('email_can_be_send')}}</p>
             </el-form-item>
-            <el-form-item label="Admin Email Address">
+            <el-form-item :label="$t('Admin Email Address')">
                 <el-input type="email" v-model="mailbox.settings.admin_email_address"></el-input>
-                <p>Please provide the email address where admin will get email if enabled in email settings</p>
+                <p>{{$t('admin_get_email')}}</p>
             </el-form-item>
-            <el-form-item label="Support Channel">
+            <el-form-item :label="$t('Support Channel')">
                 <el-radio-group v-model="mailbox.box_type">
-                    <el-radio label="web">Web Based</el-radio>
-                    <el-radio :disabled="!appVars.has_email_parser" label="email">Email Based (MailBox)</el-radio>
+                    <el-radio :label="$t('web')">{{$t('Web Based')}}</el-radio>
+                    <el-radio :disabled="!appVars.has_email_parser" label="email">{{$t('Email Based (MailBox)')}}</el-radio>
                 </el-radio-group>
             </el-form-item>
 
-            <el-form-item v-if="mailbox.box_type == 'email'" label="Mapped Email">
+            <el-form-item v-if="mailbox.box_type == 'email'" :label="$t('Mapped Email')">
                 <el-input type="email" v-model="mailbox.mapped_email"></el-input>
-                <p>Please provide mapped webhook email from where you will send emails as webhook</p>
+                <p>{{$t('mapped_webhook_email')}}</p>
             </el-form-item>
 
-            <el-form-item label="Email Footer For Customers">
+            <el-form-item :label="$t('Email Footer For Customers')">
                 <wp-editor :height="100" v-model="mailbox.email_footer"/>
                 <div>
-                    <el-button size="small" style="cursor: pointer;" @click="show_codes = !show_codes">Click to see Available Dynamic Codes:</el-button>
+                    <el-button size="small" style="cursor: pointer;" @click="show_codes = !show_codes">{{$t('see_available_dynamic_shortcodes')}}:</el-button>
                     <ul v-if="show_codes">
                         <li v-for="(code, codeName) in smartCodes">{{code}}: {{codeName}}</li>
                     </ul>
@@ -36,7 +36,7 @@
 
             <el-form-item>
                 <el-button v-loading="saving" :disabled="saving" @click="saveSettings()" type="primary">
-                    Save Settings
+                    {{$t('Save Settings')}}
                 </el-button>
             </el-form-item>
         </el-form>
@@ -56,12 +56,12 @@ export default {
         return {
             show_codes: false,
             smartCodes: {
-                '{{customer.first_name}}': 'Customer First name',
-                '{{customer.last_name}}': 'Customer Last name',
-                '{{customer.email}}': 'Customer Email',
-                '{{ticket.id}}': 'Ticket ID',
-                '{{ticket.public_url}}': 'Ticket Public URL',
-                '{{ticket.title}}': 'Ticket Title'
+                '{{customer.first_name}}': this.$t('Customer First name'),
+                '{{customer.last_name}}': this.$t('Customer Last name'),
+                '{{customer.email}}': this.$t('Customer Email'),
+                '{{ticket.id}}': this.$t('Ticket ID'),
+                '{{ticket.public_url}}': this.$t('Ticket Public URL'),
+                '{{ticket.title}}': this.$t('Ticket Title')
             },
             saving: false
         }
@@ -70,8 +70,8 @@ export default {
         filtered_client_notifications() {
             if(this.mailbox.box_type == 'email') {
                 return {
-                    ticket_created: 'Ticket Received Welcome Email',
-                    ticket_closed_by_agent: 'Ticket Closed By Agent'
+                    ticket_created: this.$t('Ticket Received Welcome Email'),
+                    ticket_closed_by_agent: this.$t('Ticket Closed By Agent')
                 }
             }
             return this.client_notifications;
