e069d2a32790c91e0f329ce778750c6283f1b360
Jewel <cep.jewel@gmail.com>
2021-10-27 15:48:53 +0600

    Fixed conflicts and move Slack to Pro

--- app/Services/Integrations/IntegrationInit.php
+++ app/Services/Integrations/IntegrationInit.php
@@ -3,13 +3,9 @@
 namespace FluentSupport\App\Services\Integrations;
 
 
-<<<<<<< HEAD
 use FluentSupport\App\Modules\IntegrationSettingsModule;
 use FluentSupport\App\Services\Integrations\Slack\SlackNotification;
-use FluentSupport\App\Services\Integrations\Telegram\TelegramNotification;
 
-=======
->>>>>>> 789d83ffe5329d1b5f8f4a837646efc349464191
 class IntegrationInit
 {
     public function init()
@@ -21,16 +17,6 @@ class IntegrationInit
         if(defined('FLUENTFORM')) {
             new \FluentSupport\App\Services\Integrations\FluentForm\FeedIntegration(wpFluentForm());
         }
-<<<<<<< HEAD
-
-        $this->addNotificationIntegrations();
     }
 
-    private function addNotificationIntegrations()
-    {
-        IntegrationSettingsModule::addIntegration(new TelegramNotification());
-        IntegrationSettingsModule::addIntegration(new SlackNotification());
-=======
->>>>>>> 789d83ffe5329d1b5f8f4a837646efc349464191
-    }
 }
