c97b2b16ee092bcf83e0a188f1cd69b0a69ef4d0
Rafi Ahmed <66135964+rafiahmedd@users.noreply.github.com>
2021-10-27 22:45:25 +0600

    Added Customer Portal Translation

--- resources/customer_portal/components/CreateTicket.vue
+++ resources/customer_portal/components/CreateTicket.vue
@@ -2,21 +2,21 @@
     <div class="fs_all_tickets">
         <div class="fs_tk_actions fs_tk_header">
             <div class="fs_tk_left">
-                <h3>Submit a Support Ticket</h3>
+                <h3>{{$t('Submit a Support Ticket')}}</h3>
             </div>
             <div class="fs_tk_right">
-                <el-button @click="$router.push({ name: 'dashboard' })" size="small" type="info">View All</el-button>
+                <el-button @click="$router.push({ name: 'dashboard' })" size="small" type="info">{{$t('View All')}}</el-button>
             </div>
         </div>
         <div style="background: white; padding: 20px;" class="fs_tk_body">
             <el-form :data="ticket" label-position="top">
-                <el-form-item label="Subject">
-                    <el-input placeholder="What's about this support ticket" type="text" v-model="ticket.title"></el-input>
+                <el-form-item :label="$t('Subject')">
+                    <el-input :placeholder="$t('What\'s about this support ticket')" type="text" v-model="ticket.title"></el-input>
                     <error :error="errors.get('title')"/>
                 </el-form-item>
-                <el-form-item label="Ticket Details">
+                <el-form-item :label="$t('Ticket Details')">
                     <wp-editor :height="150" :media-buttons="false" v-model="ticket.content" />
-                    <p>Please provide details about your problem</p>
+                    <p>{{$t('Please provide details about your problem')}}</p>
                     <error :error="errors.get('content')"/>
                 </el-form-item>
 
@@ -24,24 +24,24 @@
 
                 <div class="fs_tk_actions">
                     <div v-if="products.length" class="fs_tk_left">
-                        <el-form-item class="fs_ticket_product" label="Related Product/Service">
-                            <el-select v-model="ticket.product_id" placeholder="Select related Product/Service">
+                        <el-form-item class="fs_ticket_product" :label="$t('Related Product/Service')">
+                            <el-select v-model="ticket.product_id" :placeholder="$t('Select related Product/Service')">
                                 <el-option v-for="product in products" :key="product.id" :value="product.id" :label="product.title"></el-option>
                             </el-select>
                         </el-form-item>
                         <error :error="errors.get('product_id')"/>
                     </div>
                     <div v-if="ticket_types.length" class="fs_tk_left">
-                        <el-form-item label="Select Your Ticket Type">
-                            <el-select v-model="ticket.ticket_type_id" placeholder="Select Your Ticket Type">
+                        <el-form-item :label="$t('Select Your Ticket Type')">
+                            <el-select v-model="ticket.ticket_type_id" :placeholder="$t('Select Your Ticket Type')">
                                 <el-option v-for="ticket_type in ticket_types" :key="ticket_type.id" :value="ticket_type.id" :label="ticket_type.title"></el-option>
                             </el-select>
                         </el-form-item>
                         <error :error="errors.get('ticket_type_id')"/>
                     </div>
                     <div class="fs_tk_left">
-                        <el-form-item class="fs_ticket_priority" label="Priority">
-                            <el-select v-model="ticket.client_priority" placeholder="Select related Product/Service">
+                        <el-form-item class="fs_ticket_priority" :label="$t('Priority')">
+                            <el-select v-model="ticket.client_priority" :placeholder="$t('Select Priority')">
                                 <el-option v-for="(priority,priorityKey) in priorities" :key="priorityKey" :value="priorityKey" :label="priority"></el-option>
                             </el-select>
                         </el-form-item>
@@ -50,7 +50,7 @@
                 </div>
 
                 <el-form-item>
-                    <el-button @click="create()" :disabled="creating" v-loading="creating" type="primary">Create Ticket</el-button>
+                    <el-button @click="create()" :disabled="creating" v-loading="creating" type="primary">{{$t('Create Ticket')}}</el-button>
                 </el-form-item>
 
             </el-form>
--- resources/customer_portal/components/InlineReply.vue
+++ resources/customer_portal/components/InlineReply.vue
@@ -3,7 +3,7 @@
         <textarea @click="is_focused = true" v-if="!is_focused" class="fs_reply_text" placeholder="Click Here to Write a reply"></textarea>
 
         <div v-else class="fs_reply_wrap">
-            <h3>Write a reply</h3>
+            <h3>{{$t('Write a reply')}}</h3>
             <div>
                 <wp-editor :autofocus="true" :mediaButtons="false" :height="150" v-model="response_body" />
                 <error :error="errors.get('content')"/>
@@ -15,16 +15,16 @@
                 </div>
                 <div class="fs_half">
                     <div class="fs_response_actions">
-                        <el-checkbox class="fs_close_checkbox" v-model="close_ticket" true-label="yes" false-label="no">Close Ticket</el-checkbox>
-                        <el-button v-loading="creating" @click="reply()" size="small" type="success">Reply</el-button>
+                        <el-checkbox class="fs_close_checkbox" v-model="close_ticket" true-label="yes" false-label="no">{{$t('Close Ticket')}}</el-checkbox>
+                        <el-button v-loading="creating" @click="reply()" size="small" type="success">{{$t('Reply')}}</el-button>
                     </div>
                 </div>
             </div>
 
             <error :error="errors.get('permission_error')"/>
 
-            <p style="text-align: center;" v-if="ticket.privacy == 'private'">This ticket is <b>Private</b>. Only you and official support agents can view this conversations</p>
-            <p style="text-align: center;" v-else>This ticket is <b>Public</b>. Please do not share any private information.</p>
+            <p style="text-align: center;" v-if="ticket.privacy == 'private'">{{$t('This ticket is')}} <b>{{$t('Private')}}</b>. {{$t('agent_and_officials_can_see')}}</p>
+            <p style="text-align: center;" v-else>This ticket is <b>{{$t('Public')}}</b>. {{$t('not_to_share_private_info')}}</p>
         </div>
     </div>
 </template>
