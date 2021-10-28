e7c3446801da7f940cf78e8d7c4326fa9e72043c
Rafi Ahmed <66135964+rafiahmedd@users.noreply.github.com>
2021-10-28 17:10:34 +0600

    Added Translation to Tickets

--- app/Services/Tickets/CustomFieldsService.php
+++ app/Services/Tickets/CustomFieldsService.php
@@ -9,32 +9,32 @@ class CustomFieldsService
         $customFields = [
             [
                 'data_key' => 'ticket_type',
-                'admin_label' => 'Support Type',
-                'public_label' => 'Please let us know about this support type',
+                'admin_label' => __('Support Type', 'fluent-support'),
+                'public_label' => __('Please let us know about this support type', 'fluent-support'),
                 'type' => 'input-select',
-                'placeholder' => 'Support Type',
+                'placeholder' => __('Support Type', 'fluent-support'),
                 'options' => [
-                    'bug_report' => 'Bug Report',
-                    'feature_request' => 'Feature Request',
-                    'integration_issues' => 'Integration Issues'
+                    'bug_report' => __('Bug Report', 'fluent-support'),
+                    'feature_request' => __('Feature Request', 'fluent-support'),
+                    'integration_issues' => __('Integration Issues', 'fluent-support')
                 ],
                 'required' => true
             ],
             [
                 'data_key' => 'invoice_number',
-                'admin_label' => 'Invoice Number',
-                'public_label' => 'Please let us know your invoice number',
+                'admin_label' => __('Invoice Number', 'fluent-support'),
+                'public_label' => __('Please let us know your invoice number', 'fluent-support'),
                 'type' => 'input-text',
-                'placeholder' => 'Invoice Number',
+                'placeholder' => __('Invoice Number', 'fluent-support'),
                 'data-type' => 'text', // number / text
                 'required' => false
             ],
             [
                 'data_key' => 'details_about_issue',
-                'admin_label' => 'Details about the issue',
-                'public_label' => 'Please let us know more details about this issue',
+                'admin_label' => __('Details about the issue', 'fluent-support'),
+                'public_label' => __('Please let us know more details about this issue', 'fluent-support'),
                 'type' => 'input-textarea',
-                'placeholder' => 'Details about the issue',
+                'placeholder' => __('Details about the issue', 'fluent-support'),
                 'required' => false
             ]
         ];
