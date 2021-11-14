dc333ad5a3395b7ad37311ed738902f0d5c06c81
Jewel <cep.jewel@gmail.com>
2021-11-14 20:51:04 +0600

    Slack DONE

--- app/Services/Tickets/ResponseService.php
+++ app/Services/Tickets/ResponseService.php
@@ -4,27 +4,35 @@ namespace FluentSupport\App\Services\Tickets;
 
 use FluentSupport\App\Models\Attachment;
 use FluentSupport\App\Models\Conversation;
+use FluentSupport\Framework\Response\Response;
 use FluentSupport\Framework\Support\Arr;
 
 class ResponseService
 {
     public function createResponse($data, $person, $ticket)
     {
-        if(empty($data['content'])) {
+        if (empty($data['content'])) {
             return false;
         }
 
         $convoType = Arr::get($data, 'conversation_type', 'response');
 
+        $content = wp_unslash(wp_kses_post($data['content']));
+
         $response = [
             'person_id'         => $person->id,
             'ticket_id'         => $ticket->id,
             'conversation_type' => $convoType,
-            'content'           => wp_unslash(wp_kses_post($data['content'])),
-            'source'            => Arr::get($data, 'source', 'web')
+            'content'           => $content,
+            'source'            => Arr::get($data, 'source', 'web'),
+            'content_hash'      => md5($content)
         ];
 
-        $response = apply_filters('fluent_support/new_'.$person->person_type.'_' . $convoType, $response, $ticket, $person);
+        if(!empty($data['message_id'])) {
+            $response['message_id'] = sanitize_text_field($data['message_id']);
+        }
+
+        $response = apply_filters('fluent_support/new_' . $person->person_type . '_' . $convoType, $response, $ticket, $person);
 
         $createdResponse = \FluentSupport\App\Models\Conversation::create($response);
         $createdResponse->load('person');
@@ -34,14 +42,14 @@ class ResponseService
                 ->whereIn('file_hash', $attachments)
                 ->update([
                     'conversation_id' => $createdResponse->id,
-                    'status' => 'active'
+                    'status'          => 'active'
                 ]);
             $createdResponse->load('attachments');
         }
 
         if ($person->person_type == 'agent' && $ticket->status == 'new' && $convoType == 'response') {
             $ticket->status = 'active';
-            if($ticket->created_at) {
+            if ($ticket->created_at) {
                 $ticket->first_response_time = strtotime(current_time('mysql')) - strtotime($ticket->created_at);
             } else {
                 $ticket->first_response_time = 300;
@@ -51,7 +59,7 @@ class ResponseService
         $agentAdded = false;
         $updateData = [];
 
-        if($person->person_type == 'agent') {
+        if ($person->person_type == 'agent') {
             if (!$ticket->agent_id && $convoType == 'response') {
                 $ticket->agent_id = $person->id;
                 $agentAdded = true;
@@ -60,13 +68,13 @@ class ResponseService
                 ];
             }
 
-            if($convoType == 'response') {
+            if ($convoType == 'response') {
                 $ticket->last_agent_response = current_time('mysql');
                 $ticket->waiting_since = current_time('mysql');
             }
         } else {
 
-            if($ticket->last_agent_response && strtotime($ticket->last_agent_response) > strtotime($ticket->last_customer_response)) {
+            if ($ticket->last_agent_response && strtotime($ticket->last_agent_response) > strtotime($ticket->last_customer_response)) {
                 $ticket->waiting_since = current_time('mysql');
             }
 
@@ -83,10 +91,10 @@ class ResponseService
             $ticket->total_close_time = current_time('timestamp') - strtotime($ticket->created_at);
             $closed = true;
             Conversation::create([
-                'ticket_id' => $ticket->id,
-                'person_id' => $person->id,
+                'ticket_id'         => $ticket->id,
+                'person_id'         => $person->id,
                 'conversation_type' => 'internal_info',
-                'content' => __('Ticket has been closed', 'fluent-support')
+                'content'           => __('Ticket has been closed', 'fluent-support')
             ]);
         }
 
@@ -98,7 +106,7 @@ class ResponseService
             $updateData['agent'] = $ticket->agent;
         }
 
-        do_action('fluent_support/' . $convoType . '_added_by_'.$person->person_type, $createdResponse, $ticket, $person);
+        do_action('fluent_support/' . $convoType . '_added_by_' . $person->person_type, $createdResponse, $ticket, $person);
 
 
         if ($closed) {
@@ -107,8 +115,8 @@ class ResponseService
         }
 
         return [
-            'response' => $createdResponse,
-            'ticket' => $ticket,
+            'response'    => $createdResponse,
+            'ticket'      => $ticket,
             'update_data' => $updateData
         ];
     }
