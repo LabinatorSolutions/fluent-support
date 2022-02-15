7cc807d6eb4b74820edaa3bd92fcd1876750fde4
Rafi Ahmed <rafi201822@gmail.com>
2022-02-16 02:05:52 +0600

    Added Personal Data Erasure

--- app/Hooks/Handlers/PrivacyHandler.php
+++ app/Hooks/Handlers/PrivacyHandler.php
@@ -2,6 +2,7 @@
 
 namespace FluentSupport\App\Hooks\Handlers;
 
+use FluentSupport\App\Models\Attachment;
 use FluentSupport\App\Models\Customer;
 use FluentSupport\App\Models\Ticket;
 
@@ -10,13 +11,15 @@ class PrivacyHandler
     public function init()
     {
         add_filter( 'wp_privacy_personal_data_exporters', [$this, 'wpRegisterExporter'] );
+        add_filter( 'wp_privacy_personal_data_erasers', [$this, 'wpRegisterEraser'] );
     }
+
     public function exportPersonalData($user_email, $page = 1)
     {
         $customer = Customer::where('email', $user_email)->select(['id'])->first();
-
-//        $responseWith = $request->get('response_with', ['person', 'attachments']);
-        $tickets = Ticket::with(['customer', 'agent', 'product', 'mailbox', 'tags', 'attachments'])->where('customer_id', $customer->id)->get();
+        $tickets = Ticket::with(['customer', 'agent', 'product', 'mailbox', 'tags', 'attachments'])
+            ->where('customer_id', $customer->id)
+            ->get();
         $data = [];
         foreach ($tickets as $ticket) {
             $data[] = [
@@ -30,19 +33,19 @@ class PrivacyHandler
                     ],
                     [
                         'name' => 'Ticket Title',
-                        'value' => $ticket->title,
+                        'value' => sanitize_title($ticket->title),
                     ],
                     [
                         'name' => 'Ticket Content',
-                        'value' => $ticket->content,
+                        'value' => sanitize_textarea_field($ticket->content),
                     ],
                     [
                         'name' => 'Ticket Attachments',
-                        'value' => $ticket->attachments,
+                        'value' => $this->ticketAttachments($ticket),
                     ],
                     [
                         'name' => 'Ticket Product',
-                        'value' => $ticket->product,
+                        'value' => $ticket->product->title,
                     ],
                     [
                         'name' => 'Ticket Status',
@@ -76,11 +79,50 @@ class PrivacyHandler
      *
      * @return mixed
      */
-    public function wpRegisterExporter( $exporters ) {
+    public function wpRegisterExporter( $exporters )
+    {
         $exporters['fluent-support'] = array(
             'exporter_friendly_name' => __( 'Fluent Support Personal Data Exporter', 'fluent-support' ),
             'callback'               => [ $this, 'exportPersonalData' ],
         );
         return $exporters;
     }
+
+    public function ticketAttachments($ticket)
+    {
+        $attachments = Attachment::where('ticket_id', $ticket->id)->select(['title'])->get();
+        $text = '';
+
+        foreach ($attachments as $attachment) {
+            $text .= sanitize_title($attachment->title) . "\n";
+        }
+
+        return $text;
+    }
+
+    public function erasePersonalData($user_email, $page = 1)
+    {
+        $customer = Customer::where('email', $user_email)->select(['id'])->first();
+        $tickets = Ticket::where('customer_id', $customer->id)->get();
+        foreach ($tickets as $ticket) {
+            $ticket->deleteTicket();
+        }
+
+        $customer->delete();
+        return [
+            'items_removed' => true,
+            'items_retained' => false,
+            'messages' => [],
+            'done' => true,
+        ];
+    }
+
+     public function wpRegisterEraser( $erasers )
+     {
+         $erasers['fluent-support'] = array(
+             'eraser_friendly_name' => __( 'Fluent Support Personal Data Eraser', 'fluent-support' ),
+             'callback'             => [ $this, 'erasePersonalData' ],
+         );
+         return $erasers;
+     }
 }
