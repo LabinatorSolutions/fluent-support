70ecdefa339150eddbcb1e2e5225ab37228560bb
Rafi Ahmed <rafi201822@gmail.com>
2022-02-16 18:48:19 +0600

    Update PrivacyHandler.php

--- app/Hooks/Handlers/PrivacyHandler.php
+++ app/Hooks/Handlers/PrivacyHandler.php
@@ -2,7 +2,7 @@
 
 namespace FluentSupport\App\Hooks\Handlers;
 
-use FluentSupport\App\Models\Attachment;
+use FluentSupport\App\Models\Conversation;
 use FluentSupport\App\Models\Customer;
 use FluentSupport\App\Models\Ticket;
 
@@ -14,7 +14,8 @@ class PrivacyHandler
         add_filter( 'wp_privacy_personal_data_erasers', [$this, 'wpRegisterEraser'] );
     }
 
-    public function exportPersonalData($user_email, $page = 1)
+    // Method to handle tickets data exporter
+    public function exportTickets($user_email, $page = 1)
     {
         $customer = Customer::where('email', $user_email)->select(['id'])->first();
         $tickets = Ticket::with(['customer', 'agent', 'product', 'mailbox', 'tags', 'attachments'])
@@ -23,83 +24,231 @@ class PrivacyHandler
         $data = [];
         foreach ($tickets as $ticket) {
             $data[] = [
-                'group_id' => 'fluent-support',
-                'group_label' => 'Fluent Support',
+                'group_id' => 'fluent-support-tickets',
+                'group_label' => __('Fluent Support Tickets', 'fluent-support'),
                 'item_id' => 'ticket-' . $ticket->id,
                 'data' => [
                     [
-                        'name' => 'Ticket ID',
+                        'name' => __('Ticket ID', 'fluent-support'),
                         'value' => $ticket->id,
                     ],
                     [
-                        'name' => 'Ticket Title',
-                        'value' => sanitize_title($ticket->title),
+                        'name' => __('Ticket Title', 'fluent-support'),
+                        'value' => $ticket->title,
                     ],
                     [
-                        'name' => 'Ticket Content',
-                        'value' => sanitize_textarea_field($ticket->content),
+                        'name' => __('Ticket Content', 'fluent-support'),
+                        'value' => $ticket->content,
                     ],
                     [
-                        'name' => 'Ticket Attachments',
-                        'value' => $this->ticketAttachments($ticket),
+                        'name' => __('Ticket Attachments', 'fluent-support'),
+                        'value' => $this->ticketAttachments($ticket->attachments),
                     ],
                     [
-                        'name' => 'Ticket Product',
-                        'value' => $ticket->product->title,
+                        'name' => __('Ticket Product', 'fluent-support'),
+                        'value' => isset($ticket->product->title) ? $ticket->product->title : '',
                     ],
                     [
-                        'name' => 'Ticket Status',
+                        'name' => __('Ticket Tags', 'fluent-support'),
+                        'value' => $this->ticketTags($ticket->tags),
+                    ],
+                    [
+                        'name' => __('Ticket Status', 'fluent-support'),
                         'value' => $ticket->status,
                     ],
                     [
-                        'name' => 'Ticket Priority',
+                        'name' => __('Ticket Priority', 'fluent-support'),
                         'value' => $ticket->priority,
                     ],
                     [
-                        'name' => 'Ticket Created At',
+                        'name' => __('Ticket Created At', 'fluent-support'),
                         'value' => $ticket->created_at,
-                    ],
-                    [
-                        'name' => 'Ticket Updated At',
-                        'value' => $ticket->updated_at,
-                    ],
+                    ]
                 ],
             ];
         }
+
+        /**
+         * Filter to modify tickets exportable data
+         * @since v1.5.5
+         * @param array $data
+         * @param object $tickets
+         * @return array exportable data
+         */
+        $exportableData = apply_filters('fluent_support/make_exportable_tickets_data', $data, $tickets);
         return [
-            'data' => $data,
+            'data' => $exportableData,
             'done' => true,
         ];
     }
 
-    /**
-     * Registers all data exporters.
-     *
-     * @param array $exporters
-     *
-     * @return mixed
-     */
-    public function wpRegisterExporter( $exporters )
+    // Method to handle conversations data exporter
+    public function exportConversations($user_email, $page = 1)
     {
-        $exporters['fluent-support'] = array(
-            'exporter_friendly_name' => __( 'Fluent Support Personal Data Exporter', 'fluent-support' ),
-            'callback'               => [ $this, 'exportPersonalData' ],
-        );
-        return $exporters;
+        $customer = Customer::where('email', $user_email)->select(['id'])->first();
+        $conversations = Conversation::with(['attachments'])
+            ->where('person_id', $customer->id)
+            ->where('conversation_type', 'response')
+            ->get();
+        $data = [];
+        foreach ($conversations as $conversation) {
+            $data[] = [
+                'group_id' => 'fluent-support-conversations',
+                'group_label' => __('Fluent Support Conversations', 'fluent-support'),
+                'item_id' => 'conversation-' . $conversation->id,
+                'data' => [
+                    [
+                        'name' => __('Conversation ID', 'fluent-support'),
+                        'value' => $conversation->id,
+                    ],
+                    [
+                        'name' => __('Ticket ID', 'fluent-support'),
+                        'value' => $conversation->ticket_id,
+                    ],
+                    [
+                        'name' => __('Conversation Content', 'fluent-support'),
+                        'value' => $conversation->content,
+                    ],
+                    [
+                        'name' => __('Conversation Attachments', 'fluent-support'),
+                        'value' => $this->convAttachments($conversation->attachments),
+                    ],
+                    [
+                        'name' => __('Conversation Created At', 'fluent-support'),
+                        'value' => $conversation->created_at,
+                    ]
+                ],
+            ];
+        }
+
+        /**
+         * Filter to modify conversations exportable data
+         * @since v1.5.5
+         * @param array $data
+         * @param object $conversations
+         * @return array exportable data
+         */
+        $exportableData = apply_filters('fluent_support/make_exportable_conversations_data', $data, $conversations);
+        return [
+            'data' => $exportableData,
+            'done' => true,
+        ];
     }
 
-    public function ticketAttachments($ticket)
+    // Method to handle customers data exporter
+    public function exportCustomerData($user_email, $page = 1)
     {
-        $attachments = Attachment::where('ticket_id', $ticket->id)->select(['title'])->get();
-        $text = '';
+        $customer = Customer::where('email', $user_email)->first();
 
-        foreach ($attachments as $attachment) {
-            $text .= sanitize_title($attachment->title) . "\n";
+        $data[] = [
+            'group_id' => 'fluent-support-customer',
+            'group_label' => __('Fluent Support Customer', 'fluent-support'),
+            'item_id' => 'customer-' . $customer->id,
+            'data' => [
+                [
+                    'name' => __('Customer ID', 'fluent-support'),
+                    'value' => $customer->id,
+                ],
+                [
+                    'name' => __('First Name', 'fluent-support'),
+                    'value' => $customer->first_name,
+                ],
+                [
+                    'name' => __('Last Name', 'fluent-support'),
+                    'value' => $customer->last_name,
+                ],
+                [
+                    'name' => __('Email', 'fluent-support'),
+                    'value' => $customer->email,
+                ],
+                [
+                    'name' => __('Status', 'fluent-support'),
+                    'value' => $customer->status,
+                ],
+                [
+                    'name' => __('IP Address', 'fluent-support'),
+                    'value' => $customer->ip_address,
+                ],
+                [
+                    'name' => __('Last IP Address', 'fluent-support'),
+                    'value' => $customer->last_ip_address,
+                ],
+                [
+                    'name'  => __('Address Line One', 'fluent-support'),
+                    'value' => $customer->address_line_1
+                ],
+                [
+                    'name'  => __('Address Line Two', 'fluent-support'),
+                    'value' => $customer->address_line_2
+                ],
+                [
+                    'name'  => __('City', 'fluent-support'),
+                    'value' => $customer->city
+                ],
+                [
+                    'name'  => __('Zip', 'fluent-support'),
+                    'value' => $customer->zip
+                ],
+                [
+                    'name'  => __('State', 'fluent-support'),
+                    'value' => $customer->state
+                ],
+                [
+                    'name'  => __('Country', 'fluent-support'),
+                    'value' => $customer->country
+                ],
+                [
+                    'name' => __('Created At', 'fluent-support'),
+                    'value' => $customer->created_at
+                ],
+            ],
+        ];
+
+        /**
+         * Filter to modify customer exportable data
+         * @since v1.5.5
+         * @param array $data
+         * @param object $customer
+         * @return array exportable data
+         */
+        $exportableData = apply_filters('fluent_support/make_exportable_customer_data', $data, $customer);
+        return [
+            'data' => $exportableData,
+            'done' => true,
+        ];
+    }
+
+    public function ticketAttachments($attachments)
+    {
+        $text = '';
+        foreach ($attachments as $attachment){
+            $text .= '<a href='.esc_url($attachment->secureUrl). '>' . esc_html($attachment->title) . '</a>, <br>';
         }
 
         return $text;
     }
 
+    public function convAttachments($attachments)
+    {
+        $text = '';
+        foreach ($attachments as $attachment){
+            $text .= '<a href='.esc_url($attachment->secureUrl). '>' . esc_html($attachment->title) . '</a>, <br>';
+        }
+
+        return $text;
+    }
+
+    public function ticketTags($tags)
+    {
+        $ticketTags = '';
+        foreach ($tags as $tag) {
+            $ticketTags .= $tag->title . ', <br>';
+        }
+
+        return  $ticketTags;
+    }
+
+    // Method to handle data eraser
     public function erasePersonalData($user_email, $page = 1)
     {
         $customer = Customer::where('email', $user_email)->select(['id'])->first();
@@ -117,6 +266,27 @@ class PrivacyHandler
         ];
     }
 
+    // Registering exporters to export data
+    public function wpRegisterExporter( $exporters )
+    {
+        $exporters['fluent-support-tickets'] = array(
+            'exporter_friendly_name' => __( 'Fluent Support Tickets Exporter', 'fluent-support' ),
+            'callback'               => [ $this, 'exportTickets' ],
+        );
+
+        $exporters['fluent-support-conversations'] = array(
+            'exporter_friendly_name' => __( 'Fluent Support Conversations Exporter', 'fluent-support' ),
+            'callback'               => [ $this, 'exportConversations' ],
+        );
+
+        $exporters['fluent-support-customer'] = array(
+            'exporter_friendly_name' => __( 'Fluent Support Customer Exporter', 'fluent-support' ),
+            'callback'               => [ $this, 'exportCustomerData' ],
+        );
+        return $exporters;
+    }
+
+    // Registering erasers to erase data
      public function wpRegisterEraser( $erasers )
      {
          $erasers['fluent-support'] = array(
