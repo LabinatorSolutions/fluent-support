42e973c9d0eaf05d2306282ca43b38def34f4596
Russel Hussain <md.russel.hussain@gmail.com>
2022-04-01 12:25:06 +0600

    #Issue fixed for mentioned ticket

--- resources/admin/Modules/Tickets/_EditResponse.vue
+++ resources/admin/Modules/Tickets/_EditResponse.vue
@@ -1,8 +1,19 @@
 <template>
     <div class="fs_edit_response">
-        <wp-editor  @showSuggestion=showSuggestion v-model="response.content" />
+        <wp-editor  @showSuggestion=showSuggestion v-if="editor_ready"  v-model="response.content" />
         <hr />
-        <el-button size="small" type="success" @click="editResponse()">{{$t('Update Response')}}</el-button>
+        <el-button size="large" type="success" @click="editResponse()">{{$t('Update Response')}}</el-button>
+
+        <el-dialog v-model="popup" title="Support Stuff" width="18%" center top="45%">
+          <el-form-item>
+            <el-input v-model="agent_id" placeholder="Type to search"
+                      @keyup="searchAgent" />
+          </el-form-item>
+
+          <div class="text item"
+               v-for="agent in filteredAgents"
+               :key="agent.id" @click="tagMe(agent)">{{ agent.full_name }}</div>
+        </el-dialog>
     </div>
 </template>
 
@@ -14,7 +25,10 @@ export default {
     props: ['response'],
     data() {
         return {
-            saving: false
+            saving: false,
+            filteredAgents: this.appVars.support_agents,
+            popup: false,
+            editor_ready: true,
         }
     },
     components: {
@@ -55,12 +69,24 @@ export default {
             this.editor_ready = false;
             let newText = "@<a href='#"+agent.id+"'>"+agent.first_name+'-'+agent.last_name+"</a>";
             let RegX = new RegExp(this.replaceText+"([^"+this.replaceText+"]*)$");
-            this.response_body = this.response_body.replace(RegX, newText + '$1');
+            this.response.content = this.response.content.replace(RegX, newText + '$1');
             this.popup = false;
+
             this.$nextTick(() => {
-                this.editor_ready = true;
+              this.editor_ready = true;
             });
         }
     }
 }
 </script>
+
+<style scoped>
+.text {
+  font-size: 14px;
+}
+
+.item {
+  padding: 18px 0;
+  cursor: pointer;
+}
+</style>
