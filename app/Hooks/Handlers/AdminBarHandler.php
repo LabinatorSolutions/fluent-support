03c9ed253ea284f7a508fcd8a65a6839603d4076
Russel Hussain <md.russel.hussain@gmail.com>
2022-05-18 18:00:24 +0600

    Ticket summary in admin abr ongoing

---
+++ app/Hooks/Handlers/AdminBarHandler.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace FluentSupport\App\Hooks\Handlers;
+
+use FluentSupport\App\App;
+use FluentSupport\App\Modules\PermissionManager;
+use FluentSupport\App\Services\Tickets\TicketStats;
+
+class AdminBarHandler
+{
+    public function init()
+    {
+        $currentUserPermissions = PermissionManager::currentUserPermissions();
+
+        if (!$currentUserPermissions) {
+            return;
+        }
+
+        add_action('admin_bar_menu', [$this, 'showTicketSummary'], 999);
+
+    }
+
+    public function showTicketSummary($adminBar)
+    {
+        $app = App::getInstance();
+        $assets = $app['url.assets'];
+        wp_enqueue_script('fst_global_summary', $assets . 'admin/js/global_summary.js', ['jquery'], FLUENT_SUPPORT_VERSION);
+
+        $urlBase = apply_filters(
+            'fst_menu_url_base',
+            admin_url('admin.php?page=fluent-support#/')
+        );
+
+        wp_localize_script('fst_global_summary', 'fst_bar_vars', [
+            'rest'            => $this->getRestInfo(),
+            'links'           => (new TicketStats())->getQuickLinks(),
+            'trans' => [
+                'Quick Summary' => __('Quick Summary', 'fluent-support')
+            ]
+        ]);
+
+        $args = [
+            'parent' => 'top-secondary',
+            'id'     => 'fst_global_summary',
+            'title'  => __('Ticket Summary', 'fluent-support'),
+            'href'   => '#',
+            'meta'   => false
+        ];
+
+        $adminBar->add_node($args);
+    }
+
+    protected function getRestInfo()
+    {
+        $app = App::getInstance();
+
+        $ns = $app->config->get('app.rest_namespace');
+        $v = $app->config->get('app.rest_version');
+
+        return [
+            'base_url'  => esc_url_raw(rest_url()),
+            'url'       => rest_url($ns . '/' . $v),
+            'nonce'     => wp_create_nonce('wp_rest'),
+            'namespace' => $ns,
+            'version'   => $v
+        ];
+    }
+}
