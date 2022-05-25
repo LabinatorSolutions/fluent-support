0005fe26ca61315ba971f33c99c4165c5245a8cd
Rafi Ahmed <rafi201822@gmail.com>
2022-05-26 01:21:46 +0600

    Remove pro version code from free version

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -883,42 +883,6 @@ class TicketController extends Controller
             'rendered_fields' => \FluentSupportPro\App\Services\CustomFieldsService::getRenderedPublicFields($ticket->customer)
         ];
     }
-    /**
-     * getCustomerTickets method will return customer tickets by customer id
-     * @param Request $request
-     * @param int $customerId
-     * @return array|array[]
-     */
-    public function getCustomerTickets(Request $request, $customerId)
-    {
-        $tickets = Ticket::where('customer_id', $customerId)
-            ->where('id', '!=', $request->get('exclude_ticket_id'))
-            ->orderBy('id', 'desc')
-            ->paginate();
-
-        return [
-            'tickets' => $tickets
-        ];
-    }
-
-
-    /**
-     * mergeCustomerTickets will merge tickets into one
-     * @param Request $request
-     * @param $ticketId //ticket id where the tickets will be merged
-     * @return array|array[]
-     */
-    public function mergeCustomerTickets(Request $request, $ticketId)
-    {
-        if (!PermissionManager::currentUserCan('fst_merge_tickets')) {
-            return $this->sendError([
-                'message' => __('You do not have permission to merge tickets', 'fluent-support')
-            ]);
-        }
-
-        $ticketIDToMerge = $request->get('ticket_to_merge');
-        return (new TicketService())->mergeCustomerTickets($ticketIDToMerge, $ticketId);
-    }
 
     /**
      * syncFluentCrmTags method will synchronize the tags with Fluent CRM by contact id
