938c3cc5bde64073e25d6ee59b625068206deff6
Rafi Ahmed <rafi201822@gmail.com>
2022-06-02 14:53:05 +0600

    Refactoring customer controller

--- app/Models/Traits/CustomerTrait.php
+++ app/Models/Traits/CustomerTrait.php
@@ -2,8 +2,10 @@
 
 namespace FluentSupport\App\Models\Traits;
 
+use Exception;
 use FluentSupport\App\Models\Ticket;
 use FluentSupport\App\Services\Helper;
+use FluentSupport\Framework\Support\Arr;
 use FluentSupport\App\Services\ProfileInfoService;
 
 trait CustomerTrait
@@ -44,6 +46,60 @@ trait CustomerTrait
         ];
 
         return $this->getCustomerAdditionalData($with, $customer, $data);
+    }
+
+    /**
+     * This createCustomer method will create a new customer
+     * @param array $data
+     * @return object
+     */
+
+    public function createCustomer($data)
+    {
+        $data = Arr::only($data, $this->getFillable());
+
+        $user = get_user_by('email', $data['email']);
+
+        if ($user) {
+            $data['user_id'] = $user->ID;
+            if (empty($data['first_name'])) {
+                $data['first_name'] = $user->first_name;
+            }
+            if (empty($data['last_name'])) {
+                $data['last_name'] = $user->last_name;
+            }
+        }
+
+        return static::create($data);
+    }
+
+    /**
+     * This updateCustomer method will update a customer
+     * @param int $customerId
+     * @param array $data
+     * @return object
+     */
+
+    public function updateCustomer($customerId, $data)
+    {
+        $data = $this->takeValidKeysForUpdate($data);
+
+        $customer = static::findOrFail($customerId);
+
+        if($this->customerExists($customerId, $data['email']))
+        {
+            throw new Exception('Another Customer has same email address');
+        }
+
+        $user = get_user_by('email', $data['email']);
+
+        if ($user) {
+            $data['user_id'] = $user->ID;
+        }
+
+        static::where('id', $customer->id)->update($data);
+
+        return static::findOrFail($customerId);
     }
 
     /**
@@ -99,5 +155,36 @@ trait CustomerTrait
         }
 
         return $data;
+    }
+
+    /**
+     * This customerExists method will check if customer exists
+     * @param int $customerId
+     * @param string $email
+     * @return mixed
+     */
+
+    private function customerExists($customerId, $email)
+    {
+        $customer = static::where('id','!=', $customerId)->where('email', $email)->first();
+        if ($customer) {
+            return $customer;
+        }
+        return false;
+    }
+
+
+    /**
+     * This takeValidKeysForUpdate method will take valid keys data for update
+     * @param array $data
+     * @return array
+     */
+    private function takeValidKeysForUpdate($data)
+    {
+        $validKeys = $this->getFillable();
+        unset($validKeys['hash']);
+        unset($validKeys['user_id']);
+
+        return Arr::only($data, $validKeys);
     }
 }
