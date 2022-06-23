d1496a8f550bfa4682ed1f2069ec2938e984fa2b
Rafi Ahmed <rafi201822@gmail.com>
2022-06-23 21:06:38 +0600

    Remove unused params and namespaces

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,8 @@
 
 namespace FluentSupport\App\Http\Controllers;
 
-use FluentSupport\App\Models\Customer;
 use FluentSupport\App\Models\Product;
-use FluentSupport\App\Models\Conversation;
-use FluentSupport\App\Models\Ticket;
 use FluentSupport\Framework\Request\Request;
-use FluentSupport\Framework\Support\Arr;
 
 class ProductController extends Controller
 {
@@ -17,19 +13,18 @@ class ProductController extends Controller
      * @param Product $product
      * @return array
      */
-    public function index(Request $request, Product $product)
+    public function index ( Request $request, Product $product )
     {
       return $product->getProducts( $request->get('search') );
     }
 
     /**
      * get method will get product by id and return
-     * @param Request $request
      * @param Product $product
-     * @param $productId
+     * @param int $productId
      * @return array
      */
-    public function get(Request $request, Product $product, $productId)
+    public function get ( Product $product, $productId )
     {
         return $product->getProduct( $productId );
     }
@@ -41,7 +36,7 @@ class ProductController extends Controller
      * @return array
      * @throws \FluentSupport\Framework\Validator\ValidationException
      */
-    public function create( Request $request, Product $product )
+    public function create ( Request $request, Product $product )
     {
         $data = $request->all();
         $this->validate($data, [
@@ -72,12 +67,11 @@ class ProductController extends Controller
 
     /**
      * delete method will delete an existing product by id
-     * @param Request $request
      * @param Product $product
      * @param int $productId
      * @return array
      */
-    public function delete(Request $request, Product $product, $productId)
+    public function delete ( Product $product, $productId )
     {
         return $product->deleteProduct( $productId );
     }
