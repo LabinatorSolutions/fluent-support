83e49306509ceddd165150f30e2c9d7b7f0dab2c
Shahjahan Jewel <cep.jewel@gmail.com>
2022-08-05 15:54:20 +0600

    Add FluentCRM Create Ticket Action

--- app/Services/Integrations/IntegrationInit.php
+++ app/Services/Integrations/IntegrationInit.php
@@ -2,16 +2,12 @@
 
 namespace FluentSupport\App\Services\Integrations;
 
-
-use FluentSupport\App\Modules\IntegrationSettingsModule;
-use FluentSupport\App\Services\Integrations\Slack\SlackNotification;
-
 class IntegrationInit
 {
     public function init()
     {
         if(defined('FLUENTCRM')) {
-            (new FluentCRM())->boot();
+            (new \FluentSupport\App\Services\Integrations\FluentCrm\FluentCRMWidgets())->boot();
         }
 
         if(defined('FLUENTFORM')) {
