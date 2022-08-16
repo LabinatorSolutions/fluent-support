892f7cde7b3eea27670ee6ac61b9b205a7b7ddea
Rafi Ahmed <rafi201822@gmail.com>
2022-08-16 19:46:11 +0600

    Fix framework latest version related issues
    remove singleton binidings and WorkflowPolicy as these are not needed now

--- boot/bindings.php
+++ boot/bindings.php
@@ -6,7 +6,3 @@
  * $app
  * @var WPFluent\Foundation\Application
  */
-
-$app->singleton(\FluentSupport\App\Services\ProfileInfoService::class, function($app) {
-    return new \FluentSupport\App\Services\ProfileInfoService();
-});
