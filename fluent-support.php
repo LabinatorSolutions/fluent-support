310c4ec0b9bc357ce231788ce787febfd246dc78
Shahjahan Jewel <cep.jewel@gmail.com>
2022-10-19 17:09:31 +0600

    1.6.6 Release Ready

--- fluent-support.php
+++ fluent-support.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Fluent Support
 Description: The Ultimate Support Plugin For Your WordPress.
-Version: 1.6.5
+Version: 1.6.6
 Author: WPManageNinja LLC
 Author URI: https://wpmanageninja.com
 Plugin URI: https://fluentsupport.com
@@ -11,8 +11,8 @@ Text Domain: fluent-support
 Domain Path: /language
 */
 
-define('FLUENT_SUPPORT_VERSION', '1.6.5');
-define('FLUENT_SUPPORT_PRO_MIN_VERSION', '1.6.5');
+define('FLUENT_SUPPORT_VERSION', '1.6.6');
+define('FLUENT_SUPPORT_PRO_MIN_VERSION', '1.6.6');
 define('FLUENT_SUPPORT_UPLOAD_DIR', 'fluent-support');
 define('FLUENT_SUPPORT_PLUGIN_URL', plugin_dir_url(__FILE__));
 
