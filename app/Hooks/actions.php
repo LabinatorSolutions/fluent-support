225548085fe42686e69aee6a47587bbdeb8f81dc
Shahjahan Jewel <cep.jewel@gmail.com>
2022-10-19 17:12:47 +0600

    1.6.6 Ready

--- app/Hooks/actions.php
+++ app/Hooks/actions.php
@@ -1,15 +1,6 @@
 <?php
 
 /**
- * All registered action's handlers should be in app\Hooks\Handlers,
- * addAction is similar to add_action and addCustomAction is just a
- * wrapper over add_action which will add a prefix to the hook name
- * using the plugin slug to make it unique in all wordpress plugins,
- * ex: $app->addCustomAction('foo', ['FooHandler', 'handleFoo']) is
- * equivalent to add_action('slug-foo', ['FooHandler', 'handleFoo']).
- */
-
-/**
  * @var $app FluentSupport\Framework\Foundation\Application
  */
 
--- app/Hooks/filters.php
+++ app/Hooks/filters.php
@@ -1,35 +1,12 @@
 <?php
 
 /**
- * All registered filter's handlers should be in app\Hooks\Handlers,
- * addFilter is similar to add_filter and addCustomFlter is just a
- * wrapper over add_filter which will add a prefix to the hook name
- * using the plugin slug to make it unique in all wordpress plugins,
- * ex: $app->addCustomFilter('foo', ['FooHandler', 'handleFoo']) is
- * equivalent to add_filter('slug-foo', ['FooHandler', 'handleFoo']).
- */
-
-/**
  * @var $app \FluentSupport\Framework\Foundation\Application;
  */
-
 
 add_filter('fluent_support/parse_smartcode_data', function ($string, $data) {
     return (new \FluentSupport\App\Services\Parser\Parser())->parse($string, $data);
 }, 10, 2);
-
-
-/*
- * In the WP core wp-includes/functions.php file, where the filter is defined for the list of mime types and file extensions
- * In the list the JSON file type/extension is missing. So we had to add this application/JSON type to the list by the hooks
- */
-
-add_filter('mime_types', function($mimes) {
-    $mimes['json'] = 'application/json';
-
-    return $mimes;
-});
-
 
 add_filter('fluent_support/dashboard_notice', function ($messages) {
     if(defined('FLUENTSUPPORTPRO_PLUGIN_VERSION') && version_compare(FLUENT_SUPPORT_PRO_MIN_VERSION, FLUENTSUPPORTPRO_PLUGIN_VERSION, '>')) {
