9bcd289ec7e35d1956e3fb1567143fa76b617720
bijit027 <debbijit70@gmail.com>
2022-12-15 09:40:17 +0600

    Dashboard design ongoing and and add translate

--- resources/admin/Modules/Dashboard/TicketStatistics.vue
+++ resources/admin/Modules/Dashboard/TicketStatistics.vue
@@ -9,33 +9,39 @@
                 <b>{{ stat.title }}: </b> {{ stat.count }}
             </li>
         </ul>
-        <p class="fs_stat_helper" v-if="component_data.individual_stat" >
-            <span style="color:#FF7C7C">
+        <p class="fs_stat_helper" v-if="component_data.individual_stat">
+            <span style="color: #ff7c7c">
                 {{ component_data.individual_stat.waiting_tickets }}
-                {{ $t("tickets") }}
+                {{ translate("tickets") }}
             </span>
-            {{ $t("are waiting for reply with") }}
-            <span style="color:#88C379">
-                {{ $t("average") }}
+            {{ translate("are waiting for reply with") }}
+            <span style="color: #88c379">
+                {{ translate("average") }}
                 {{ component_data.individual_stat.average_waiting }}
-                {{ $t("wait time") }}</span
-            >
-            & {{ $t("max wait time") }}
-            <span style="color:#7280FF">{{ component_data.individual_stat.max_waiting }}</span>
+                {{ translate("wait time") }}
+            </span>
+            & {{ translate("max wait time") }}
+            <span style="color: #7280ff">{{
+                component_data.individual_stat.max_waiting
+            }}</span>
         </p>
     </div>
 </template>
 <script>
+import { useFluentHelper } from "@/admin/Composable/FluentFrameworkHelper";
 export default {
     props: ["component_data"],
     name: "TicketStatistics",
     setup() {
-        return {};
+        const { translate } = useFluentHelper();
+        return {
+            translate,
+        };
     },
 };
 </script>
 <style scoped>
-    .fs_stat_helper{
-        padding: 5px 30px;
-    }
+.fs_stat_helper {
+    padding: 5px 30px;
+}
 </style>
