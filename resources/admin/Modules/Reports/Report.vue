52c3494ec71739c8b84a0ac8c60b4eab0e70da82
bijit027 <debbijit70@gmail.com>
2022-12-27 14:42:10 +0600

    Apply Composition API in Report and AgentReports

--- resources/admin/Modules/Reports/Report.vue
+++ resources/admin/Modules/Reports/Report.vue
@@ -1,9 +1,9 @@
 <template>
     <el-tabs v-model="activeName">
-        <el-tab-pane :label="$t('Personal Reports')" name="my-reports" :lazy="true">
+        <el-tab-pane :label="translate('Personal Reports')" name="my-reports" :lazy="true">
             <personal-reports :url="'my-reports'" />
         </el-tab-pane>
-        <el-tab-pane v-if="me.permissions.indexOf('fst_sensitive_data') != -1" :label="$t('Agents Reports')" name="reports" :lazy="true">
+        <el-tab-pane v-if="me.permissions.indexOf('fst_sensitive_data') != -1" :label="translate('Agents Reports')" name="reports" :lazy="true">
             <reports/>
         </el-tab-pane>
     </el-tabs>
@@ -12,14 +12,29 @@
 <script type="text/babel">
 import Reports from "./Reports";
 import PersonalReports from "./PersonalReports";
+import {
+    useFluentHelper,
+} from "@/admin/Composable/FluentFrameworkHelper";
+import { reactive, toRefs } from "vue";
 
 export default {
     name: 'Report',
     components:{ Reports, PersonalReports },
-    data() {
-        return {
+
+    setup(){
+        const {
+            appVars,
+            translate,
+        } = useFluentHelper();
+
+        const state = reactive({
             activeName: 'my-reports',
-            me: this.appVars.me
+            me: appVars.me
+        });
+
+        return {
+            ...toRefs(state),
+            translate
         }
     }
 }
