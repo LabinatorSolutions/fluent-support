4005f9086381a77ccb8278f724fab8671432c30e
bijit027 <debbijit70@gmail.com>
2022-12-27 17:01:37 +0600

    Composition API in Reports and  PersonalReports

--- resources/admin/Modules/Reports/PersonalReports.vue
+++ resources/admin/Modules/Reports/PersonalReports.vue
@@ -6,7 +6,15 @@
                     <div class="fs_box">
                         <div class="fs_box_header">
                             <div class="fs_header_title">
-                                <el-dropdown style="display: inline-block; cursor: pointer; line-height: 100%;" @command="handleComponentChange" trigger="hover">
+                                <el-dropdown
+                                    style="
+                                        display: inline-block;
+                                        cursor: pointer;
+                                        line-height: 100%;
+                                    "
+                                    @command="handleComponentChange"
+                                    trigger="hover"
+                                >
                                     <span class="el-dropdown-link">
                                         {{ chartMaps[currently_showing] }}
                                         <el-icon> <ArrowDown /> </el-icon>
@@ -14,7 +22,9 @@
                                     <template #dropdown>
                                         <el-dropdown-menu>
                                             <el-dropdown-item
-                                                v-for="(mapName, mapKey) in chartMaps"
+                                                v-for="(
+                                                    mapName, mapKey
+                                                ) in chartMaps"
                                                 :key="mapKey"
                                                 :command="mapKey"
                                             >
@@ -26,19 +36,24 @@
                                 <el-date-picker
                                     v-model="date_range"
                                     type="daterange"
-                                    :range-separator="$t('To')"
-                                    :start-placeholder="$t('Start')"
-                                    :end-placeholder="$t('End')"
-                                    :unlink-panels=true
+                                    :range-separator="translate('To')"
+                                    :start-placeholder="translate('Start')"
+                                    :end-placeholder="translate('End')"
+                                    :unlink-panels="true"
                                     @change="filterReport"
                                     value-format="YYYY-MM-DD"
-                                    style="max-width: 350px;"
+                                    style="max-width: 350px"
                                 >
                                 </el-date-picker>
                             </div>
                         </div>
                         <div class="fs_box_body">
-                            <component v-if="showing_charts" :is="currently_showing" :date_range="date_range" :url="'my-reports'"></component>
+                            <component
+                                v-if="showing_charts"
+                                :is="currently_showing"
+                                :date_range="date_range"
+                                :url="'my-reports'"
+                            ></component>
                         </div>
                     </div>
                     <agent-reports :url="'my-reports/my-summary'" />
@@ -47,35 +62,46 @@
                     <div class="fs_box">
                         <div class="fs_box_header">
                             <div class="fs_header_title">
-                                {{$t('Your Overall Stats')}}
+                                {{ translate("Your Overall Stats") }}
                             </div>
                         </div>
                         <div class="fs_box_body">
                             <ul v-if="!loading" class="fs_card_list">
-                                <li style="padding: 15px;" v-for="(stat, stat_type) in overall_reports" :key="stat_type">
-                                    <b>{{stat.title}}:</b>  {{stat.count}}
+                                <li
+                                    style="padding: 15px"
+                                    v-for="(stat, stat_type) in overall_reports"
+                                    :key="stat_type"
+                                >
+                                    <b>{{ stat.title }}:</b> {{ stat.count }}
                                 </li>
                             </ul>
                             <div class="fs_padded_20" v-else>
-                                <el-skeleton :rows="3" animated/>
+                                <el-skeleton :rows="3" animated />
                             </div>
                         </div>
                     </div>
 
-                    <div class="fs_box fs_today_stats" style="margin-top: 1.2500em">
+                    <div
+                        class="fs_box fs_today_stats"
+                        style="margin-top: 1.25em"
+                    >
                         <div class="fs_box_header">
                             <div class="fs_header_title">
-                                {{$t('Today Stats')}}
+                                {{ translate("Today Stats") }}
                             </div>
                         </div>
                         <div class="fs_box_body">
                             <ul v-if="!loading" class="fs_card_list">
-                                <li style="padding: 15px;" v-for="(stat, stat_type) in today_reports" :key="stat_type">
-                                    <b>{{stat.title}}:</b>  {{stat.count}}
+                                <li
+                                    style="padding: 15px"
+                                    v-for="(stat, stat_type) in today_reports"
+                                    :key="stat_type"
+                                >
+                                    <b>{{ stat.title }}:</b> {{ stat.count }}
                                 </li>
                             </ul>
                             <div class="fs_padded_20" v-else>
-                                <el-skeleton :rows="3" animated/>
+                                <el-skeleton :rows="3" animated />
                             </div>
                         </div>
                     </div>
@@ -89,63 +115,78 @@
 import ResponseChart from "./Charts/ResponseGrowth";
 import ResolveChart from "./Charts/ResolveGrowth";
 import AgentReports from "./AgentReports";
+import { useFluentHelper } from "@/admin/Composable/FluentFrameworkHelper";
+import { reactive, toRefs, onMounted } from "vue";
 
 export default {
     name: "PersonalReports",
-    props: ['url'],
+    props: ["url"],
     components: {
         ResponseChart,
         ResolveChart,
-        AgentReports
+        AgentReports,
     },
-    data() {
-        return {
+
+    setup(props) {
+        const { get, translate, handleError, nextTick, setTitle, appVars } =
+            useFluentHelper();
+
+        const state = reactive({
             loading: false,
             stat_loading: false,
             overall_reports: {},
-            currently_showing: 'resolve-chart',
-            date_range: ['', ''],
+            currently_showing: "resolve-chart",
+            date_range: ["", ""],
             showing_charts: true,
             chartMaps: {
-                'resolve-chart': 'Resolve Stats',
-                'response-chart': 'Response Stats'
+                "resolve-chart": "Resolve Stats",
+                "response-chart": "Response Stats",
             },
-            me: this.appVars.me,
-            today_reports: {}
-        }
-    },
-    methods: {
-        fetchReports() {
-            this.loading = true;
-            this.$get(this.url)
-                .then(response => {
-                    this.overall_reports = response.overall_reports;
-                    this.today_reports = response.today_reports;
+            me: appVars.me,
+            today_reports: {},
+        });
+
+        const fetchReports = async () => {
+            state.loading = true;
+            await get(props.url)
+                .then((response) => {
+                    state.overall_reports = response.overall_reports;
+                    state.today_reports = response.today_reports;
                 })
                 .catch((errors) => {
-                    this.$handleError(errors);
+                    handleError(errors);
                 })
                 .always(() => {
-                    this.loading = false;
+                    state.loading = false;
                 });
-        },
-        handleComponentChange(item) {
-            this.currently_showing = item;
-        },
-        filterReport() {
-            const current = this.currently_showing;
-            this.currently_showing = {
-                render: () => {
-                }
+        };
+
+        const handleComponentChange = (item) => {
+            state.currently_showing = item;
+        };
+
+        const filterReport = () => {
+            const current = state.currently_showing;
+            state.currently_showing = {
+                render: () => {},
             };
-            this.$nextTick(() => {
-                this.currently_showing = current;
+            nextTick(() => {
+                state.currently_showing = current;
             });
-        }
+        };
+
+        onMounted(() => {
+            fetchReports();
+            setTitle("Personal Reports");
+        });
+
+        return {
+            ...toRefs(state),
+            translate,
+            fetchReports,
+            handleComponentChange,
+            filterReport,
+        };
     },
-    mounted() {
-        this.fetchReports();
-        this.$setTitle('Personal Reports');
-    }
-}
+};
 </script>
