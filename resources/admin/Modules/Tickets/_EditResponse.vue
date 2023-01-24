5a5c5cf3a28036f85e4204ee0f108840b7c6a4e7
Russel Hussain <md.russel.hussain@gmail.com>
2023-01-24 11:29:23 +0600

    Edit response converted into composition API

--- resources/admin/Modules/Tickets/_EditResponse.vue
+++ resources/admin/Modules/Tickets/_EditResponse.vue
@@ -8,42 +8,69 @@
 
 <script type="text/babel">
 import WpEditor from '../../Pieces/_wp_editor';
+import {reactive, toRefs} from "vue";
+import {useConfirm, useFluentHelper, useNotify} from "@/admin/Composable/FluentFrameworkHelper";
 
 export default {
     name: 'EditResponse',
     props: ['response'],
-    data() {
-        return {
-            saving: false,
-            filteredAgents: this.appVars.support_agents,
-            popup: false,
-            editor_ready: true,
-        }
-    },
     components: {
         WpEditor
     },
-    emits: ['updated'],
-    methods: {
-        editResponse() {
-            this.saving = true;
-            this.$put(`tickets/${this.response.ticket_id}/responses/${this.response.id}`, {
-                content: this.response.content
+    setup(props, context){
+        const emit = context.emit;
+        const { notify } = useNotify();
+        const { confirm } = useConfirm();
+        const {
+            appVars,
+            get,
+            post,
+            put,
+            translate,
+            handleError,
+            has_pro,
+        } = useFluentHelper();
+        const state = reactive({
+            saving: false,
+            filteredAgents: appVars.support_agents,
+            popup: false,
+            editor_ready: true
+        });
+
+        const editResponse = () => {
+            state.saving = true;
+            put(`tickets/${props.response.ticket_id}/responses/${props.response.id}`, {
+                content: props.response.content
             })
                 .then(response => {
-                    this.$notify.success({
+                    notify.success({
                         message: response.message,
                         position: 'bottom-right',
                         type: 'success'
                     });
-                    this.$emit('updated', response.response);
+                    emit('updated', response.response);
                 })
                 .catch((errors) => {
-                    this.$handleError(errors);
+                    handleError(errors);
                 })
                 .always(() => {
-                    this.saving = false;
+                    state.saving = false;
                 });
+        }
+
+        return {
+            ...toRefs(state),
+            notify,
+            confirm,
+            appVars,
+            get,
+            post,
+            put,
+            translate,
+            handleError,
+            has_pro,
+            emit,
+            editResponse
         }
     }
 }
