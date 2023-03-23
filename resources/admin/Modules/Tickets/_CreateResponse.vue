5133d4e0f2dc86f9c04f5dcaaf7c3223937959df
bijit027 <debbijit70@gmail.com>
2023-03-23 11:55:06 +0600

    Implement composition API in Ticket response

--- resources/admin/Modules/Tickets/_CreateResponse.vue
+++ resources/admin/Modules/Tickets/_CreateResponse.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="fs_create_response" :class="'fs_reply_type_'+type">
-        <wp-editor :autofocus="true" v-if="editor_ready" v-model="response_body" :show-shortcodes="true" :show-saved-replies="true"/>
+        <wp-editor :autofocus="true" v-if="editor_ready" v-model="response_body" :show-shortcodes="true"
+                   :show-saved-replies="true"/>
         <div class="fs_row">
             <div class="fs_half">
                 <div style="text-align: left" class="fs_response_actions">
-                    <el-button v-loading="creating" :disabled="creating" @click="create('no')" size="large" type="success">
-                        <span v-if="type== 'note'">{{ $t('Add Internal Note') }}</span>
-                        <span v-else>{{ $t('Add Reply') }}</span>
+                    <el-button v-loading="creating" :disabled="creating" @click="create('no')" size="large"
+                               type="success">
+                        <span v-if="type== 'note'">{{ translate('Add Internal Note') }}</span>
+                        <span v-else>{{ translate('Add Reply') }}</span>
                     </el-button>
-                    <el-button v-if="type != 'note'" :disabled="creating" @click="create('yes')" size="large" type="danger">
-                        {{ $t('Reply and Close') }}
+                    <el-button v-if="type != 'note'" :disabled="creating" @click="create('yes')" size="large"
+                               type="danger">
+                        {{ translate('Reply and Close') }}
                     </el-button>
-                    <p v-if="type== 'note'">{{ $t('internal_note_warning') }}</p>
+                    <p v-if="type== 'note'">{{ translate('internal_note_warning') }}</p>
                 </div>
             </div>
             <div class="fs_half">
@@ -23,6 +26,11 @@
 
 <script type="text/babel">
 
+import {reactive, toRefs} from "vue";
+import {
+    useFluentHelper,
+    useNotify,
+} from "@/admin/Composable/FluentFrameworkHelper";
 import WpEditor from '../../Pieces/_wp_editor';
 import AttachmentForm from './_AttachmentForm';
 
@@ -33,60 +41,74 @@ export default {
         WpEditor,
         AttachmentForm
     },
-    data() {
-        return {
+
+    setup(props, {emit}) {
+
+        const {post, translate, handleError} = useFluentHelper();
+        const {notify} = useNotify();
+
+        const state = reactive({
             response_body: '',
             creating: false,
             close_ticket: 'no',
             attachments: [],
             editor_ready: true,
             shortcodes: {
-                '{{customer.first_name}}' : 'Customer First Name',
-                '{{customer.last_name}}' : 'Customer Last Name',
-                '{{customer.full_name}}' : 'Customer Full Name',
-                '{{customer.email}}' : 'Customer Email',
-                '{{agent.first_name}}' : 'Agent First Name',
-                '{{agent.last_name}}' : 'Agent Last Name',
-                '{{agent.full_name}}' : 'Agent Full Name',
-                '{{agent.email}}' : 'Agent Email',
+                '{{customer.first_name}}': 'Customer First Name',
+                '{{customer.last_name}}': 'Customer Last Name',
+                '{{customer.full_name}}': 'Customer Full Name',
+                '{{customer.email}}': 'Customer Email',
+                '{{agent.first_name}}': 'Agent First Name',
+                '{{agent.last_name}}': 'Agent Last Name',
+                '{{agent.full_name}}': 'Agent Full Name',
+                '{{agent.email}}': 'Agent Email',
             }
-        }
-    },
-    methods: {
-        create(closed = 'no') {
+        });
+
+        const create = (closed = 'no') => {
 
             const data = {
-                content: this.response_body,
-                conversation_type: this.type,
+                content: state.response_body,
+                conversation_type: props.type,
                 close_ticket: closed,
-                attachments: this.attachments
+                attachments: state.attachments
             };
 
-            let action = `tickets/${this.ticket.id}/responses`;
-            if (Array.isArray(this.ticket)) {
-                data.ticket_ids = this.ticket;
+            let action = `tickets/${props.ticket.id}/responses`;
+            if (Array.isArray(props.ticket)) {
+                data.ticket_ids = props.ticket;
                 data.bulk_action = 'reply_tickets';
                 action = 'tickets/bulk-reply';
             }
 
-            this.creating = true;
-            this.$post(action, data)
+            state.creating = true;
+            post(action, data)
                 .then(response => {
-                    this.$notify.success({
+                    notify({
                         message: response.message,
+                        type: 'success',
                         position: 'bottom-right'
                     });
-                    this.response_body = '';
-                    this.$emit('created', response.response, response);
-                    this.attachments = [];
+                    state.response_body = '';
+                    emit('created', response.response, response);
+                    state.attachments = [];
                 })
                 .catch((errors) => {
-                    this.$handleError(errors);
+                    handleError(errors);
                 })
                 .always(() => {
-                    this.creating = false;
+                    state.creating = false;
                 });
         }
+
+        return {
+            ...toRefs(state),
+            create,
+            translate,
+        };
     }
 }
 </script>
+
+
+
