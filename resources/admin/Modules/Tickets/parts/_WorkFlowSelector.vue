4286abd142f684d11e9f5fb2c8219a97dcee60b4
bijit027 <debbijit70@gmail.com>
2023-03-27 14:53:17 +0600

    _WorkflowSelector.vue file convert into composition API

--- resources/admin/Modules/Tickets/parts/_WorkFlowSelector.vue
+++ resources/admin/Modules/Tickets/parts/_WorkFlowSelector.vue
@@ -6,7 +6,9 @@
             trigger="click"
         >
             <template #reference>
-               <el-icon style="vertical-align: middle;"><Lightning /></el-icon>
+                <el-icon style="vertical-align: middle;">
+                    <Lightning/>
+                </el-icon>
             </template>
 
             <ul class="fs_workflows_list">
@@ -19,14 +21,15 @@
         <el-dialog
             v-model="modalVisible"
             :append-to-body="true"
-            :title="$t('Review your Workflow')"
+            :title="translate('Review your Workflow')"
             width="60%"
         >
             <div v-if="modalVisible" v-loading="fetchingActions" class="fs_workflow_desc">
-                <h3>{{$t('Running the')}} <b>{{ selected_workflow.title }}</b> {{$t('workflow will perform the following actions:')}}</h3>
+                <h3>{{ translate('Running the') }} <b>{{ selected_workflow.title }}</b>
+                    {{ translate('workflow will perform the following actions:') }}</h3>
                 <ul class="fs_workflow_items">
                     <li v-for="(task,taskIndex) in workflow_actions" :key="task.id">
-                        <span v-if="taskIndex != 0">{{$t('and')}}</span> {{ task.title || task.action_name }}
+                        <span v-if="taskIndex != 0">{{ translate('and') }}</span> {{ task.title || task.action_name }}
                     </li>
                 </ul>
             </div>
@@ -34,7 +37,7 @@
             <template #footer>
                 <span class="dialog-footer">
                     <el-button v-loading="applying" :disabled="applying || fetchingActions" type="primary"
-                               @click="runWorkFow()">{{$t('Run Workflow')}}</el-button>
+                               @click="runWorkFow()">{{ translate('Run Workflow') }}</el-button>
                 </span>
             </template>
         </el-dialog>
@@ -43,65 +46,90 @@
 </template>
 
 <script type="text/babel">
+import {reactive, toRefs} from "vue";
+import {useRouter} from 'vue-router'
+import {useFluentHelper, useNotify} from "@/admin/Composable/FluentFrameworkHelper";
+
 export default {
     name: 'WorkflowRunner',
     props: ['ticket_ids'],
-    data() {
-        return {
-            workflows: this.appVars.manual_workflows,
+
+    setup(props, {emit}) {
+        const {
+            translate,
+            handleError,
+            appVars,
+            post,
+            get
+        } = useFluentHelper();
+        const {notify} = useNotify();
+        const router = useRouter()
+
+        const state = reactive({
+            workflows: appVars.manual_workflows,
             selected_workflow: false,
             visible: false,
             modalVisible: false,
             workflow_actions: [],
             fetchingActions: false,
             applying: false
-        }
-    },
-    methods: {
-        showWorkflowModal(workflow) {
-            this.selected_workflow = workflow;
-            this.fetchActions();
-            this.visible = false;
-            this.modalVisible = true;
-        },
-        fetchActions() {
-            this.workflow_actions = [];
-            this.fetchingActions = true;
-            this.$get('workflows/' + this.selected_workflow.id + '/actions')
+        });
+
+        const showWorkflowModal = (workflow) => {
+            state.selected_workflow = workflow;
+            fetchActions();
+            state.visible = false;
+            state.modalVisible = true;
+        };
+
+        const fetchActions = () => {
+            state.workflow_actions = [];
+            state.fetchingActions = true;
+            get('workflows/' + state.selected_workflow.id + '/actions')
                 .then((response) => {
-                    this.workflow_actions = response.actions;
+                    state.workflow_actions = response.actions;
                 })
                 .catch((errors) => {
-                    this.$handleError(errors);
+                    handleError(errors);
                 })
                 .always(() => {
-                    this.fetchingActions = false;
+                    state.fetchingActions = false;
                 });
-        },
-        runWorkFow() {
-            this.applying = true;
-            this.$post('workflows/' + this.selected_workflow.id + '/run', {
-                ticket_ids: this.ticket_ids
+        };
+
+        const runWorkFow = () => {
+            state.applying = true;
+            post('workflows/' + state.selected_workflow.id + '/run', {
+                ticket_ids: props.ticket_ids
             })
                 .then(response => {
-                    this.$notify.success({
+                    notify({
                         message: response.message,
-                        position: 'bottom-right'
+                        position: 'bottom-right',
+                        type: 'success'
                     });
-                    this.modalVisible = false;
+                    state.modalVisible = false;
                     // if `workflow_action` contains `fs_delete_ticket` on `action_name` then redirect to tickets list
-                    if (this.workflow_actions.some(action => action.action_name === 'fs_delete_ticket')) {
-                        this.$router.push({name: 'tickets'});
+                    if (state.workflow_actions.some(action => action.action_name === 'fs_delete_ticket')) {
+                        router.push({name: 'tickets'});
                     } else {
-                        this.$emit('reloadTickets');
+                        emit('reloadTickets');
                     }
                 })
                 .catch((errors) => {
-                    this.$handleError(errors);
+                    handleError(errors);
                 })
                 .always(() => {
-                    this.applying = false;
+                    state.applying = false;
                 });
+        }
+
+        return {
+            ...toRefs(state),
+            showWorkflowModal,
+            fetchActions,
+            runWorkFow,
+            translate
         }
     }
 }
