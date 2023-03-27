9ea86408f9fc0b74862ecaf168917cf9167b0615
bijit027 <debbijit70@gmail.com>
2023-03-27 12:32:01 +0600

    _AttachmentForm.vue file convert into composition API

--- resources/admin/Modules/Tickets/_AttachmentForm.vue
+++ resources/admin/Modules/Tickets/_AttachmentForm.vue
@@ -15,9 +15,9 @@
             :file-list="file_lists"
             list-type="picture"
         >
-            <el-button size="default" type="primary">{{$t('Click to upload')}}</el-button>
+            <el-button size="default" type="primary">{{ translate('Click to upload') }}</el-button>
             <template #tip>
-                <div class="el-upload__tip">{{$t('allowed_files_and_size')}}</div>
+                <div class="el-upload__tip">{{ translate('allowed_files_and_size') }}</div>
             </template>
         </el-upload>
         <p style="color: red;" v-if="error_message" @click="error_message = ''">{{ error_message }}</p>
@@ -26,61 +26,82 @@
 </template>
 
 <script type="text/babel">
+import {reactive, toRefs} from "vue";
+import {useFluentHelper} from "@/admin/Composable/FluentFrameworkHelper";
+
 export default {
     name: 'AttachmentForm',
     props: ['ticket', 'attachments'],
-    data() {
-        return {
-            upload_url: this.appVars.rest.url+'/ticket_file_upload',
+
+    setup(props) {
+        const {
+            translate,
+            appVars,
+            convertToText
+        } = useFluentHelper();
+
+        const state = reactive({
+            upload_url: appVars.rest.url + '/ticket_file_upload',
             upload_data: {
-                ticket_id: this.ticket.id,
-                intended_ticket_hash: this.appVars.intended_ticket_hash
+                ticket_id: props.ticket.id,
+                intended_ticket_hash: appVars.intended_ticket_hash
             },
             file_lists: [],
             requestHeaders: {
-                'X-WP-Nonce': this.appVars.rest.nonce
+                'X-WP-Nonce': appVars.rest.nonce
             },
             error_message: '',
             dialogImageUrl: '',
             dialogImageTitle: '',
             dialogVisible: false,
-        }
-    },
-    methods: {
-        handleRemove(file, fileList) {
-            this.error_message = '';
-            this.attachments.splice(this.attachments.indexOf(file.response.attachments[0]), 1);
-        },
-        handleExceed(files,fileList) {
-            this.error_message = `${this.$t('You can upload maximum')} ${fileList.length} ${this.$t('files')}`;
-        },
-        handleError(err, file, fileList) {
-            let message = this.convertToText(JSON.parse(err.message));
-            if(!message) {
-                message = this.$t('File failed to upload');
+        });
+
+        const handleRemove = (file, fileList) => {
+            state.error_message = '';
+            props.attachments.splice(props.attachments.indexOf(file.response.attachments[0]), 1);
+        };
+
+        const handleExceed = (files, fileList) => {
+            state.error_message = `${translate('You can upload maximum')} ${fileList.length} ${translate('files')}`;
+        };
+
+        const handleError = (err, file, fileList) => {
+            let message = convertToText(JSON.parse(err.message));
+            if (!message) {
+                message = translate('File failed to upload');
             }
 
             message = file.name + ': ' + message;
-            this.error_message = message;
-        },
-        handleUploadSuccess(response, file, fileList) {
-            this.error_message = '';
-            this.attachments.push(...response.attachments);
+            state.error_message = message;
+        };
 
-            if(file.raw.type.includes('application') || file.raw.type.includes('text')) {
-                if(this.appVars.asset_url) {
-                    return file.url = this.appVars.asset_url+'images/icons/file.svg';
+        const handleUploadSuccess = (response, file, fileList) => {
+            state.error_message = '';
+            props.attachments.push(...response.attachments);
+
+            if (file.raw.type.includes('application') || file.raw.type.includes('text')) {
+                if (appVars.asset_url) {
+                    return file.url = appVars.asset_url + 'images/icons/file.svg';
                 }
-                return file.url = this.appVars.fallback_image;
+                return file.url = appVars.fallback_image;
             }
+        }
+
+        return {
+            ...toRefs(state),
+            handleRemove,
+            handleExceed,
+            handleError,
+            handleUploadSuccess,
+            translate,
         }
     }
 }
 </script>
 
 <style scoped>
-.el-dialog__body img{
-  width: 50%;
-  margin-left: 12em;
+.el-dialog__body img {
+    width: 50%;
+    margin-left: 12em;
 }
 </style>
