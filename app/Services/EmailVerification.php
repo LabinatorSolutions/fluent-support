fe14d9ee11e026e30db48db7193005ca305254c4
Russel Hussain <md.russel.hussain@gmail.com>
2023-03-28 09:40:22 +0600

    Small typeo updated

--- app/Services/EmailVerification.php
+++ app/Services/EmailVerification.php
@@ -45,8 +45,8 @@ class EmailVerification
         Helper::updateOption($email.'_verification_code', $verificationCode);
 
         $firstName = Helper::getCurrentCustomer()->first_name;
-        $emailText = sprintf('<p>Hello %s,</p><br> <p>Please click on the link below to verify your email address.</p><br>', $firstName, $verificationLink);
-        $emailText .= sprintf('<a href="%s" style="text-decoration: none;"><button style="background: #0fb779; color: #fff; border: none; padding: 12px; border-radius: 2px; cursor: pointer;">Click Here</button></a><br>', $verificationLink, $verificationLink);
+        $emailText = sprintf('<p>Hello %s,</p><br> <p>Please click on the link below to verify your email address.</p><br>', $firstName);
+        $emailText .= sprintf('<a href="%s" style="text-decoration: none;"><button style="background: #0fb779; color: #fff; border: none; padding: 12px; border-radius: 2px; cursor: pointer;">Click Here</button></a><br>', $verificationLink);
         $emailText .= sprintf('<p>Alternatively, you can use the below code to verify:</p><br>');
         $emailText .= sprintf('<div style="background:#e9e9e9;padding: 10px;text-align: center;letter-spacing: 14px;font-size: 20px;border: 1px dashed;"><strong>%s</strong></div><br>', $verificationCode);
         $emailText .= sprintf('<p>If you didn\'t request this verification, please ignore this email. This verification code will be expired in one hour.</p><br>');
