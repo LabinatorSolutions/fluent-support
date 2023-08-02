ca3bad9a69e9bf312ec565c2c4bcef411b338efc
bijit027 <debbijit70@gmail.com>
2023-08-02 14:14:22 +0600

    Email rtl feature added

--- app/Views/emails/ticket_template.php
+++ app/Views/emails/ticket_template.php
@@ -15,12 +15,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 </head>
 <body>
-<div class="fs_comment">
-    <?php echo $email_body;  // WPCS: XSS ok. ?>
-</div>
+    <div class="fs_comment <?php echo is_rtl() ? 'rtl-align' : ''; ?>">
+        <?php echo $email_body;  // Make sure to properly sanitize $email_body. ?>
+    </div>
 
-<div style="color: #9e9e9e; margin: 10px 0 14px 0; padding-top: 10px;border-top: 1px solid #eeeeee;">
-    <?php echo $email_footer;  // WPCS: XSS ok. ?>
-</div>
+    <div style="color: #9e9e9e; margin: 10px 0 14px 0; padding-top: 10px;border-top: 1px solid #eeeeee;">
+        <?php echo $email_footer;  // WPCS: XSS ok. ?>
+    </div>
 </body>
 </html>
