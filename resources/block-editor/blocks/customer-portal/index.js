617b543a6fa2949920ab537a69074e35d53f5251
Russel Hussain <md.russel.hussain@gmail.com>
2023-09-05 18:56:23 +0600

    Skeleton ongoing for all tickets

--- resources/block-editor/blocks/customer-portal/index.js
+++ resources/block-editor/blocks/customer-portal/index.js
@@ -2,14 +2,18 @@ const { registerBlockType } = wp.blocks;
 const { __ } = wp.i18n;
 import Edit from './edit';
 import attributes from './attributes';
-import metadata from './block.json';
 import icons from '../../icons';
-registerBlockType( metadata.name, {
+registerBlockType( 'fluent-support/customer-portal', {
+    apiVersion: 2,
+    version: "0.1.0",
     title: __('Customer Portal'),
     description: __('Add the Customer Portal from where user will submit tickets and manage their tickets.'),
     textdomain: 'fluent-support',
     category: 'design',
     icon: icons.fluentSupport,
+    /**
+     * @see ./attributes.js
+     */
     attributes,
     /**
      * @see ./edit.js
