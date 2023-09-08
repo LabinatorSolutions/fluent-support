6b9d675766c1735fdd9e9993bf4912b119a8739c
bijit027 <debbijit70@gmail.com>
2023-09-08 13:15:17 +0600

    Refactor tickets list template

--- resources/block-editor/blocks/customer-portal/edit.js
+++ resources/block-editor/blocks/customer-portal/edit.js
@@ -1,9 +1,10 @@
-const { Fragment, useState, useEffect} = wp.element;
+const {Fragment, useState, useEffect} = wp.element;
 //Import styles
 import './editor.scss';
 //Import Inspector controls
 import Inspector from './inspector';
-export default function Edit( { attributes, setAttributes } ) {
+
+export default function Edit({attributes, setAttributes}) {
     const ticketHeaderStyle = {
         backgroundColor: attributes.ticketHeaderBgColor,
     }
@@ -63,6 +64,7 @@ export default function Edit( { attributes, setAttributes } ) {
         setShowTicket(true);
     }
 
+
     const refreshButtonStyle = {
         padding: '3px 10px',
         backgroundColor: attributes.refreshButtonBgColor,
@@ -112,140 +114,151 @@ export default function Edit( { attributes, setAttributes } ) {
         background: attributes.threadContentBgColor,
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         setShowTickets(true);
-    },true);
+    }, true);
 
     return (
         <Fragment>
-            <Inspector attributes={attributes} setAttributes={setAttributes} />
+            <Inspector attributes={attributes} setAttributes={setAttributes}/>
             {showTickets === true ?
                 <div className={'block-wrapper'}>
-                <div className={'ticket-header'} style={ticketHeaderStyle}>
-                    <div className={'ticket-header-left'} style={ticketHeaderLeftStyle}>
-                        <button style={allBtnStyle}>
-                            All
-                        </button>
-                        <button style={openBtnStyle}>
-                            Open
-                        </button>
-                        <button style={closedBtnStyle}>
-                            Closed
-                        </button>
+                    <div className={"all-tickets-header"}>
+                        <div class="all-tickets-header-left">
+                            <div class="all-tickets-button-groups">
+                                <button className="all-tickets btn-all btn-active">All</button>
+                                <button className="all-tickets btn-open">Open</button>
+                                <button className="all-tickets btn-closed">Closed</button>
+                            </div>
+                        </div>
+                        <div className="all-tickets-header-right">
+                            <button className="all-tickets btn-logout">Logout</button>
+                            <button className="all-tickets btn-success btn-create-ticket">Create a New Ticket</button>
+                        </div>
                     </div>
-                    <div className={'ticket-header-right'} style={ticketHeaderRightStyle}>
-                        <button style={createTicketBtnStyle} onClick={showCreateTicket}>
-                            Create a New Ticket
-                        </button>
-                    </div>
-                </div>
-                <div className={'ticket-tables'}>
-                    <table className={'ticket-table'}>
-                        <thead className={'ticket-table-header'}>
+                    <div className={"all-tickets-body"}>
+                        <table className="all-tickets-table table_stripe">
+                            <thead>
                             <tr>
                                 <th>Conversation</th>
+                                <th></th>
                                 <th>Status</th>
                                 <th>Date</th>
                             </tr>
-                        </thead>
-                        <tbody>
+                            </thead>
+                            <tbody>
                             <tr onClick={viewTicket}>
-                                <td>1</td>
-                                <td>Test Ticket</td>
-                                <td>General</td>
+                                <td><a className={"ticket-preview"}><strong>Test ticket 1</strong>
+                                    <div className="prev_text_parent"><p className="ticket-preview-text">Test ticket
+                                        1</p>
+                                    </div>
+                                </a></td>
+                                <td className="ticket-thread-count"><span className="thread-count">0</span></td>
+                                <td className="ticket-status"><span
+                                    className="ticket-status-content">new</span></td>
+                                <td className="fs_tk_date"><span className="fs_tk_date">20 hours ago</span></td>
                             </tr>
                             <tr onClick={viewTicket}>
-                                <td>2</td>
-                                <td>Test Ticket</td>
-                                <td>General</td>
+                                <td><a className={"ticket-preview"}><strong>Test ticket 2</strong>
+                                    <div className="prev_text_parent"><p className="ticket-preview-text">Test ticket
+                                        2</p>
+                                    </div>
+                                </a></td>
+                                <td className="ticket-thread-count"><span className="thread-count">0</span></td>
+                                <td className="ticket-status"><span
+                                    className="ticket-status-content">new</span></td>
+                                <td className="fs_tk_date"><span className="fs_tk_date">19 hours ago</span></td>
                             </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div className={'ticket-footer'}>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="all-tickets-footer"></div>
 
                 </div>
-            </div>
-            : showForm === true ?
-                <div className={'block-wrapper'}>
-                    <div className={'create-ticket-header'}>
-                        <div className={'create-ticket-header-left'}>
-                            <h3>Submit a Support Ticket</h3>
+                : showForm === true ?
+                    <div className={'block-wrapper'}>
+                        <div className={'create-ticket-header'}>
+                            <div className={'create-ticket-header-left'}>
+                                <h3>Submit a Support Ticket</h3>
+                            </div>
+                            <div className={'create-ticket-header-right'}>
+                                <button onClick={() => showTicketsList()}>View All</button>
+                            </div>
                         </div>
-                        <div className={'create-ticket-header-right'}>
-                            <button onClick={() => showTicketsList()}>View All</button>
-                        </div>
-                    </div>
-                    <div className={'create-ticket-body'}>
-                        <div className={'create-ticket-body-left'}>
-                            <div className={'create-ticket-body-left-header'}>
-                                <h3>General</h3>
+                        <div className={'create-ticket-body'}>
+                            <div className={'create-ticket-body-left'}>
+                                <div className={'create-ticket-body-left-header'}>
+                                    <h3>General</h3>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            : showTicket === true ?
-                <div className={'block-wrapper'}>
-                    <div className={'show-ticket-header'} style={viewTicketHeaderStyle}>
-                        <div className={'show-th-header'}>
-                            <hgroup>
-                                <div className={'show-tk-subject'}>
-                                    <h2 title="show-tk-edit-subject">
-                                        <span className="show-ticket-id">#746</span> Check htttp
-                                    </h2>
-                                    <div className={"show-tk-tags"}>
-                                        <span className={"show-badge show-badge-new"} >
+                    : showTicket === true ?
+                        <div className={'block-wrapper'}>
+                            <div className={'show-ticket-header'} style={viewTicketHeaderStyle}>
+                                <div className={'show-th-header'}>
+                                    <hgroup>
+                                        <div className={'show-tk-subject'}>
+                                            <h2 title="show-tk-edit-subject">
+                                                <span className="show-ticket-id">#746</span> Check htttp
+                                            </h2>
+                                            <div className={"show-tk-tags"}>
+                                        <span className={"show-badge show-badge-new"}>
                                             new
                                         </span>
-                                    </div>
-                                </div>
-                                <div className={"show-tk-actions"}>
-                                    <button style={refreshButtonStyle} >
-                                        &#8635;
-                                    </button>
-                                    <a
-                                        style={allButtonStyle}
-                                    >
-                                        All
-                                    </a>
-                                    <button
-                                        style={closeTicketButton}
-                                    >
-                                        Close Ticket
-                                    </button>
-                                </div>
-                            </hgroup>
-                        </div>
-                    </div>
-                    <div className={'show-ticket-body'}>
-                        <div className={'show-reply-box'} style={replyBoxStyle}>
-                            <textarea className="show-reply-text" placeholder="Click Here to Write a reply"></textarea>
-                        </div>
-                        <div className="show-threads-container">
-                            <article className="show-thread conversion-starter">
-                                <div className="show-thread-content" style={threadContentStyle}>
-                                    <section className="show-avatar">
-                                        <img src="https://secure.gravatar.com/avatar/?s=96&amp;d=mm&amp;r=g" alt="auth test" />
-                                    </section>
-                                    <section className="show-thread-wrap">
-                                        <section className="show-thread-message">
-                                            <div className="show-thread-head">
-                                                <div className="show-thread-title">
-                                                    <strong>You</strong> started the conversation
-                                                </div>
-                                                <div className="show-thread-actions">2023-08-30T09:32:16+00:00</div>
                                             </div>
-                                            <div className="show-thread-body">
-                                                <p>Check htttp</p>
-                                            </div>
-                                        </section>
-                                    </section>
+                                        </div>
+                                        <div className={"show-tk-actions"}>
+                                            <button style={refreshButtonStyle}>
+                                                &#8635;
+                                            </button>
+                                            <a
+                                                onClick={showTicketsList}
+                                                style={allButtonStyle}
+                                            >
+                                                All
+                                            </a>
+                                            <button
+                                                style={closeTicketButton}
+                                            >
+                                                Close Ticket
+                                            </button>
+                                        </div>
+                                    </hgroup>
                                 </div>
-                            </article>
+                            </div>
+                            <div className={'show-ticket-body'}>
+                                <div className={'show-reply-box'} style={replyBoxStyle}>
+                                    <textarea className="show-reply-text"
+                                              placeholder="Click Here to Write a reply"></textarea>
+                                </div>
+                                <div className="show-threads-container">
+                                    <article className="show-thread conversion-starter">
+                                        <div className="show-thread-content" style={threadContentStyle}>
+                                            <section className="show-avatar">
+                                                <img src="https://secure.gravatar.com/avatar/?s=96&amp;d=mm&amp;r=g"
+                                                     alt="auth test"/>
+                                            </section>
+                                            <section className="show-thread-wrap">
+                                                <section className="show-thread-message">
+                                                    <div className="show-thread-head">
+                                                        <div className="show-thread-title">
+                                                            <strong>You</strong> started the conversation
+                                                        </div>
+                                                        <div className="show-thread-actions">2023-08-30T09:32:16+00:00
+                                                        </div>
+                                                    </div>
+                                                    <div className="show-thread-body">
+                                                        <p>Check htttp</p>
+                                                    </div>
+                                                </section>
+                                            </section>
+                                        </div>
+                                    </article>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            : null}
+                        : null}
         </Fragment>
     );
 }
