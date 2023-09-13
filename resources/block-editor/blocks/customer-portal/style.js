8e3ca0879d6ade315623acc36cb6c27f42bc3882
Russel Hussain <md.russel.hussain@gmail.com>
2023-09-13 13:23:44 +0600

    style file seperated from the editor

---
+++ resources/block-editor/blocks/customer-portal/style.js
@@ -0,0 +1,34 @@
+// styles.js
+export const getAllTicketsHeaderStyle = (attributes) => ({
+    background: attributes.allTicketsHeaderBgColor,
+});
+
+export const getAllTicketsAllButtonStyle = (attributes) => ({
+    color: attributes.filterButtonAllTextColor,
+    background: attributes.filterButtonAllBgColor,
+});
+
+export const getAllTicketsOpenButtonStyle = (attributes) => ({
+    color: attributes.filterButtonOpenTextColor,
+    background: attributes.filterButtonOpenBgColor,
+});
+
+export const getAllTicketsClosedButtonStyle = (attributes) => ({
+    color: attributes.filterButtonClosedTextColor,
+    background: attributes.filterButtonClosedBgColor,
+});
+
+export const getAllTicketsLogoutButtonStyle = (attributes) => ({
+    color: attributes.allTicketsLogoutButtonTextColor,
+    background: attributes.allTicketsLogoutButtonBgColor,
+});
+
+export const getAllTicketsCreateTicketButtonStyle = (attributes) => ({
+    color: attributes.buttonCreateTicketTextColor,
+    background: attributes.buttonCreateTicketBgColor,
+});
+
+export const getAllTicketsFooterStyle = (attributes)  =>({
+    background: attributes.allTicketsFooterBgColor,
+});
+
