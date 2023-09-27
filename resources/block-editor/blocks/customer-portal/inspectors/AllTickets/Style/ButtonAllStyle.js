21e52b9e77f3589edce0e1d5067aae6cb2ce8cbc
Russel Hussain <md.russel.hussain@gmail.com>
2023-09-27 19:12:32 +0600

    Predefined color selection option added

--- resources/block-editor/blocks/customer-portal/inspectors/AllTickets/Style/ButtonAllStyle.js
+++ resources/block-editor/blocks/customer-portal/inspectors/AllTickets/Style/ButtonAllStyle.js
@@ -5,10 +5,22 @@ export default function ButtonAllStyle({ attributes, setAttributes}) {
         <PanelBody title={__('All', 'fluent-support')}>
             <p><strong>{__('Background Color', 'fluent-support')}</strong></p>
             <ColorPalette value={attributes.filterButtonAllBgColor}
+                          colors={[
+                              { name: 'green', color: '#67C23A' },
+                              { name: 'black', color: '#000000' },
+                              { name: 'gray', color: '#777777' },
+                              { name: 'red', color: '#ff0000' },
+                          ]}
                           onChange={(v) => setAttributes({ filterButtonAllBgColor: v })}
             />
             <p><strong>{__('Text Color', 'fluent-support')}</strong></p>
             <ColorPalette value={attributes.filterButtonAllTextColor}
+                          colors={[
+                              { name: 'green', color: '#67C23A' },
+                              { name: 'black', color: '#000000' },
+                              { name: 'gray', color: '#777777' },
+                              { name: 'red', color: '#ff0000' },
+                          ]}
                           onChange={(v) => setAttributes({ filterButtonAllTextColor: v })}
             />
         </PanelBody>
--- resources/block-editor/blocks/customer-portal/inspectors/AllTickets/Style/ButtonCreateTicket.js
+++ resources/block-editor/blocks/customer-portal/inspectors/AllTickets/Style/ButtonCreateTicket.js
@@ -2,13 +2,25 @@ const { __ } = wp.i18n;
 const { PanelBody, ColorPalette } = wp.components;
 export default function ButtonCreateTicketStyle({ attributes, setAttributes}) {
     return (
-        <PanelBody title={__('Create a New Ticket', 'fluent-support')} initialOpen={ false }>
+        <PanelBody title={__('Create a New Ticket', 'fluent-support')}>
             <p><strong>{__('Background Color', 'fluent-support')}</strong></p>
             <ColorPalette value={attributes.buttonCreateTicketBgColor}
+                          colors={[
+                                { name: 'green', color: '#67C23A' },
+                                { name: 'black', color: '#000000' },
+                                { name: 'gray', color: '#777777' },
+                                { name: 'red', color: '#ff0000' },
+                          ]}
                           onChange={(v) => setAttributes({ buttonCreateTicketBgColor: v })}
             />
             <p><strong>{__('Text Color', 'fluent-support')}</strong></p>
             <ColorPalette value={attributes.buttonCreateTicketTextColor}
+                          colors={[
+                              { name: 'green', color: '#67C23A' },
+                              { name: 'black', color: '#000000' },
+                              { name: 'gray', color: '#777777' },
+                              { name: 'red', color: '#ff0000' },
+                          ]}
                           onChange={(v) => setAttributes({ buttonCreateTicketTextColor: v })}
             />
         </PanelBody>
