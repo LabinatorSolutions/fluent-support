7b774a48db9cb81095e5c58e8e901395c0b6ebac
bijit027 <debbijit70@gmail.com>
2023-10-05 15:57:40 +0600

    Added all tickets custom style in customer portal

--- resources/block-editor/blocks/customer-portal/inspectors/AllTickets/Advanced/ButtonLogoutAdvanced.js
+++ resources/block-editor/blocks/customer-portal/inspectors/AllTickets/Advanced/ButtonLogoutAdvanced.js
@@ -5,16 +5,16 @@ export default function ButtonLogoutAdvanced({attributes, setAttributes}) {
         <PanelBody title={__('Logout', 'fluent-support')}>
             <p><strong>{__('Border Width', 'fluent-support')}</strong></p>
             <RangeControl
-                value={attributes.filterButtonLogoutBorderWidth}
-                onChange={(v) => setAttributes({filterButtonLogoutBorderWidth: v})}
+                value={attributes.allTicketsLogoutButtonBorderWidth}
+                onChange={(v) => setAttributes({allTicketsLogoutButtonBorderWidth: v})}
                 min={1}
                 max={5}
             />
 
             <p><strong>{__('Border Radius', 'fluent-support')}</strong></p>
             <RangeControl
-                value={attributes.filterButtonLogoutBorderRadius}
-                onChange={(v) => setAttributes({filterButtonLogoutBorderRadius: v})}
+                value={attributes.allTicketsLogoutButtonBorderRadius}
+                onChange={(v) => setAttributes({allTicketsLogoutButtonBorderRadius: v})}
                 min={0}
                 max={15}
             />
