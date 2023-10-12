6ab774f650ae1be50c49867c7d0df7a52ed8ddea
Russel Hussain <md.russel.hussain@gmail.com>
2023-10-12 15:17:52 +0600

    Refactored BlockHelper

--- app/Services/BlockHelper.php
+++ app/Services/BlockHelper.php
@@ -2,9 +2,12 @@
 
 namespace FluentSupport\App\Services;
 
-//use function GuzzleHttp\default_ca_bundle;
 class BlockHelper
 {
+
+    // Store the attributes provided for styling
+    private static $attributes = [];
+
     public static function CustomerPortalBlockAttributes()
     {
         return [
@@ -885,51 +888,60 @@ class BlockHelper
         ];
     }
 
+    /**
+     * Generate and output inline CSS styles based on the provided attributes.
+     *
+     * This method processes the provided attributes and dynamically generates CSS styles for various components
+     * of customer portal pages, such as the Tickets List, View Ticket, and Create Ticket sections.
+     *
+     * @param array $attributes An associative array of attributes used for styling.
+     */
     public static function processAttributesAndPrepareStyle($attributes)
     {
+        self::$attributes = $attributes;
         ?>
         <style type="text/css">
             #fluent_support_client_app {
             /*Tickets List style*/
                 .fs_button_groups > .fs_btn_active {
-                    border-color: <?php echo esc_attr($attributes['filterButtonActiveBorderColor'] ?? '#409eff'); ?>;
-                    box-shadow: -1px 0 0 0 <?php echo esc_attr($attributes['filterButtonActiveBorderColor'] ?? '#409eff'); ?>;
-                    background-color: <?php echo esc_attr($attributes['filterButtonActiveBgColor'] ?? '#409eff'); ?>;
-                    color: <?php echo esc_attr($attributes['filterButtonActiveTextColor'] ?? '#ffffff'); ?>;
+                    border-color: <?php echo self::getPrintableColor('filterButtonActiveBorderColor', '#409eff'); ?>;
+                    box-shadow: -1px 0 0 0 <?php echo self::getPrintableColor('filterButtonActiveBorderColor', '#409eff'); ?>;
+                    background-color: <?php echo self::getPrintableColor('filterButtonActiveBgColor', '#409eff'); ?>;
+                    color: <?php echo self::getPrintableColor('filterButtonActiveTextColor', '#ffffff');?>;
                 }
                 .fst_tickets .fs_all_tickets .fs_tk_header {
-                    background-color: <?php echo esc_attr($attributes['allTicketsHeaderBgColor'] ?? '#ebeef4'); ?>;
+                    background-color: <?php echo self::getPrintableColor('allTicketsHeaderBgColor', '#ebeef4'); ?>;
                     <?php echo self::getBorderRadiusStyle('allTicketsHeader', $attributes)?>
                 }
                 .fs_btn_all {
-                    background-color: <?php echo esc_attr($attributes['filterButtonAllBgColor'] ?? '#409eff'); ?>;
-                    color: <?php echo esc_attr($attributes['filterButtonAllTextColor'] ?? '#ffffff'); ?>;
-                    box-shadow: 1px 0 0 0 <?php echo esc_attr($attributes['filterButtonAllBgColor'] ?? '#409eff'); ?>;
-                    <?php echo self::getBorderStyle('filterButtonAllBorder', $attributes)?>
+                    background-color: <?php echo self::getPrintableColor('filterButtonAllBgColor', '#409eff');?>;
+                    color: <?php echo self::getPrintableColor('filterButtonAllTextColor', '#ffffff');?>;
+                    box-shadow: 1px 0 0 0 <?php self::getPrintableColor('filterButtonAllBgColor', '#409eff'); ?>;
+                    <?php echo self::getBorderStyle('filterButtonAllBorder')?>
                     <?php echo self::getBorderRadiusStyle('filterButtonAllBorder', $attributes)?>
                 }
                 .fs_btn_open {
-                    background-color: <?php echo esc_attr($attributes['filterButtonOpenBgColor'] ?? '#ffffff'); ?>;
-                    color: <?php echo esc_attr($attributes['filterButtonOpenTextColor'] ?? '#606266'); ?>;
-                    <?php echo self::getBorderStyle('filterButtonOpenBorder', $attributes)?>
+                    background-color: <?php echo self::getPrintableColor('filterButtonOpenBgColor', '#ffffff'); ?>;
+                    color: <?php echo self::getPrintableColor('filterButtonOpenTextColor', '#606266'); ?>;
+                    <?php echo self::getBorderStyle('filterButtonOpenBorder')?>
                     <?php echo self::getBorderRadiusStyle('filterButtonOpenBorder', $attributes)?>
                 }
                 .fs_btn_closed {
-                    background-color: <?php echo esc_attr($attributes['filterButtonClosedBgColor']); ?>;
-                    color: <?php echo esc_attr($attributes['filterButtonClosedTextColor']); ?>;
-                    <?php echo self::getBorderStyle('filterButtonClosedBorder', $attributes)?>
+                    background-color: <?php echo self::getPrintableColor('filterButtonClosedBgColor', '#ffffff');?>;
+                    color: <?php echo self::getPrintableColor('filterButtonClosedTextColor', '#606266'); ?>;
+                    <?php echo self::getBorderStyle('filterButtonClosedBorder')?>
                     <?php echo self::getBorderRadiusStyle('filterButtonClosedBorder', $attributes)?>
                 }
                 .fs_btn_logout {
                     background-color: <?php echo esc_attr($attributes['allTicketsLogoutButtonBgColor']); ?>;
                     color: <?php echo esc_attr($attributes['allTicketsLogoutButtonTextColor']); ?>;
-                    <?php echo self::getBorderStyle('allTicketsLogoutButtonBorder', $attributes)?>
+                    <?php echo self::getBorderStyle('allTicketsLogoutButtonBorder')?>
                     <?php echo self::getBorderRadiusStyle('allTicketsLogoutButtonBorder', $attributes)?>
                 }
                 .fs_btn_create_ticket {
                     background-color:<?php echo esc_attr($attributes['buttonCreateTicketBgColor']); ?>;
                     color:<?php echo esc_attr($attributes['buttonCreateTicketTextColor']); ?>;
-                    <?php echo self::getBorderStyle('allTicketsCreateTicketButtonBorder', $attributes)?>
+                    <?php echo self::getBorderStyle('allTicketsCreateTicketButtonBorder')?>
                     <?php echo self::getBorderRadiusStyle('allTicketsCreateTicketButtonBorder', $attributes)?>
                 }
 
@@ -953,7 +965,7 @@ class BlockHelper
             /*View Ticket style*/
             .fst_client_portal .fs_ticket .fs_tk_header {
                 background-color: <?php echo esc_attr($attributes['viewTicketHeaderStyleBgColor']); ?>;
-                <?php echo self::getBorderStyle('viewTicketHeaderBorder', $attributes)?>
+                <?php echo self::getBorderStyle('viewTicketHeaderBorder')?>
                 <?php echo self::getBorderRadiusStyle('viewTicketHeaderBorder', $attributes)?>
             }
 
@@ -968,21 +980,21 @@ class BlockHelper
             .fs_refresh_button {
                 background-color: <?php echo esc_attr($attributes['refreshButtonBgColor']); ?>;
                 color: <?php echo esc_attr($attributes['refreshButtonTextColor']); ?>;
-                <?php echo self::getBorderStyle('refreshButtonBorder', $attributes)?>
+                <?php echo self::getBorderStyle('refreshButtonBorder')?>
                 <?php echo self::getBorderRadiusStyle('refreshButtonBorder', $attributes)?>
             }
 
             .fs_all_button {
                 background-color: <?php echo esc_attr($attributes['allButtonBgColor'] ?? '#ffffff'); ?>;
                 color: <?php echo esc_attr($attributes['allButtonTextColor']); ?>;
-                <?php echo self::getBorderStyle('allButtonBorder', $attributes)?>
+                <?php echo self::getBorderStyle('allButtonBorder')?>
                 <?php echo self::getBorderRadiusStyle('allButtonBorder', $attributes)?>
             }
 
             .fs_close_button {
                 background-color: <?php echo esc_attr($attributes['closeTicketButtonBgColor'] ?? '#f56c6c'); ?>;
                 color: <?php echo esc_attr($attributes['closeTicketButtonTextColor']); ?>;
-                <?php echo self::getBorderStyle('closeTicketButtonBorder', $attributes)?>
+                <?php echo self::getBorderStyle('closeTicketButtonBorder')?>
                 <?php echo self::getBorderRadiusStyle('closeTicketButtonBorder', $attributes)?>
             }
 
@@ -1024,7 +1036,7 @@ class BlockHelper
             /*Create Ticket style*/
             .fs_tk_create_head {
                 background-color:  <?php echo esc_attr($attributes['createTicketHeaderBgColor']); ?>;
-                <?php echo self::getBorderStyle('createTicketHeaderBorder', $attributes)?>
+                <?php echo self::getBorderStyle('createTicketHeaderBorder')?>
                 <?php echo self::getBorderRadiusStyle('createTicketHeader', $attributes)?>
             }
 
@@ -1035,7 +1047,7 @@ class BlockHelper
             .fs_tk_right .fs_view_all_button {
                 background-color:  <?php echo esc_attr($attributes['createTicketViewAllButtonBgColor']); ?>;
                 color: <?php echo esc_attr($attributes['createTicketViewAllButtonTextColor']); ?>;
-                <?php echo self::getBorderStyle('createTicketViewAllButtonBorder', $attributes)?>
+                <?php echo self::getBorderStyle('createTicketViewAllButtonBorder')?>
                 <?php echo self::getBorderRadiusStyle('createTicketViewAllButtonBorder', $attributes)?>
             }
 
@@ -1054,35 +1066,109 @@ class BlockHelper
             .fs_attachment_button {
                 background-color:  <?php echo esc_attr($attributes['createTicketUploadButtonBgColor']); ?>;
                 color: <?php echo esc_attr($attributes['createTicketUploadButtonTextColor']); ?>;
-                <?php echo self::getBorderStyle('createTicketUploadButtonBorder', $attributes)?>
+                <?php echo self::getBorderStyle('createTicketUploadButtonBorder')?>
                 <?php echo self::getBorderRadiusStyle('createTicketUploadButtonBorder', $attributes)?>
             }
 
             .fs_create_button {
                 background-color:  <?php echo esc_attr($attributes['createTicketCreateButtonBgColor']); ?>;
                 color: <?php echo esc_attr($attributes['createTicketCreateButtonTextColor']); ?>;
-                <?php echo self::getBorderStyle('createTicketCreateButtonBorder', $attributes)?>
+                <?php echo self::getBorderStyle('createTicketCreateButtonBorder')?>
                 <?php echo self::getBorderRadiusStyle('createTicketCreateButtonBorder', $attributes)?>
             } }
         </style>
     <?php }
 
-    public static function getBorderStyle($parentName, $attributes)
+    /**
+     * Decide whether a color attribute is printable. If not, return the default color.
+     *
+     * This function checks if the provided color attribute is empty or null. If it is, it returns the default color.
+     * Otherwise, it returns the color attribute.
+     *
+     * @param string|null $colorAttribute The color attribute to check.
+     * @param string $defaultColor The default color to use if the attribute is not printable.
+     * @return string The printable color or the default color.
+     */
+    public static function getPrintableColor($colorAttribute, $defaultColor) {
+        if( !self::isThisItemPrintAble($colorAttribute)){
+            return $defaultColor;
+        }
+        return esc_attr(self::$attributes[$colorAttribute]);
+    }
+
+    /**
+     * Check if a attribute is printable.
+     *
+     * This method checks whether a specific attribute has a value that should be included in
+     * the generated CSS.
+     *
+     * @param string $attribute The name of the border-related attribute to check.
+     * @return bool True if the attribute is printable, false otherwise.
+     */
+    public static function isThisItemPrintAble($attribute)
+    {
+        return isset(self::$attributes[$attribute]) && (self::$attributes[$attribute] != '' && self::$attributes[$attribute] != 0);
+    }
+
+    /**
+     * Generate CSS for border properties (top, right, bottom, left).
+     *
+     * This method generates CSS for border properties based on the provided attribute values, including
+     * border width and border color for each side (top, right, bottom, left).
+     *
+     * @param string $parentName The base name of the border-related attributes.
+     * @return string The generated CSS for the border properties.
+     */
+    public static function getBorderStyle($parentName)
     {
         $borderStyle = '';
-        $borderStyle .= 'border-top: ' . $attributes[$parentName . 'WidthTop'] . 'px solid ' . $attributes[$parentName . 'Color'] . ';';
-        $borderStyle .= 'border-right: ' . $attributes[$parentName . 'WidthRight'] . 'px solid ' . $attributes[$parentName . 'Color'] . ';';
-        $borderStyle .= 'border-bottom: ' . $attributes[$parentName . 'WidthBottom'] . 'px solid ' . $attributes[$parentName . 'Color'] . ';';
-        $borderStyle .= 'border-left: ' . $attributes[$parentName . 'WidthLeft'] . 'px solid ' . $attributes[$parentName . 'Color'] . ';';
+        $BorderTop = $parentName . 'WidthTop';
+        $borderColor = self::getPrintableColor($parentName . 'Color', '#ffffff');
+        if( self::isThisItemPrintAble($BorderTop) ) {
+            $borderStyle .= 'border-top: ' . esc_attr(self::$attributes[$BorderTop]) . 'px solid ' . $borderColor . ';';
+        }
+        $borderRight = $parentName . 'WidthRight';
+        if( self::isThisItemPrintAble($borderRight) ) {
+            $borderStyle .= 'border-right: ' . esc_attr(self::$attributes[$borderRight]) . 'px solid ' . $borderColor . ';';
+        }
+        $borderBottom = $parentName . 'WidthBottom';
+        if( self::isThisItemPrintAble($borderRight) ) {
+            $borderStyle .= 'border-bottom: ' . esc_attr(self::$attributes[$borderBottom]) . 'px solid ' . $borderColor . ';';
+        }
+        $borderLeft = $parentName . 'WidthLeft';
+        if( self::isThisItemPrintAble($borderLeft) ) {
+            $borderStyle .= 'border-left: ' . esc_attr(self::$attributes[$borderLeft]) . 'px solid ' . $borderColor . ';';
+        }
         return $borderStyle;
     }
 
-    public static function getBorderRadiusStyle($parentName, $attributes){
+    /**
+     * Generate CSS for border radius properties (top-left, top-right, bottom-right, bottom-left).
+     *
+     * This method generates CSS for border radius properties based on the provided attribute values, including
+     * radius values for each corner (top-left, top-right, bottom-right, bottom-left).
+     *
+     * @param string $parentName The base name of the border radius-related attributes.
+     * @return string The generated CSS for the border radius properties.
+     */
+    public static function getBorderRadiusStyle($parentName){
         $borderRadius = '';
-        $borderRadius .= 'border-top-left-radius: ' . $attributes[$parentName . 'RadiusTopLeft'] . 'px;';
-        $borderRadius .= 'border-top-right-radius: ' . $attributes[$parentName . 'RadiusTopRight'] . 'px;';
-        $borderRadius .= 'border-bottom-right-radius: ' . $attributes[$parentName . 'RadiusBottomRight'] . 'px;';
-        $borderRadius .= 'border-bottom-left-radius: ' . $attributes[$parentName . 'RadiusBottomLeft'] . 'px;';
+        $topLeft = $parentName . 'RadiusTopLeft';
+        if( self::isThisItemPrintAble($topLeft) ) {
+            $borderRadius .= 'border-top-left-radius: ' . self::$attributes[$topLeft] . 'px;';
+        }
+        $topRight = $parentName . 'RadiusTopRight';
+        if( self::isThisItemPrintAble($topRight) ) {
+            $borderRadius .= 'border-top-right-radius: ' . self::$attributes[$topRight] . 'px;';
+        }
+        $bottomRight = $parentName . 'RadiusBottomRight';
+        if( self::isThisItemPrintAble($bottomRight) ) {
+            $borderRadius .= 'border-bottom-right-radius: ' . self::$attributes[$bottomRight] . 'px;';
+        }
+        $bottomLeft = $parentName . 'RadiusBottomLeft';
+        if( self::isThisItemPrintAble($bottomLeft) ) {
+            $borderRadius .= 'border-bottom-left-radius: ' . self::$attributes[$bottomLeft] . 'px;';
+        }
         return $borderRadius;
     }
 }
