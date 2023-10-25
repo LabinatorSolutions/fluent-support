6f02c3355eee49328621f6a743d4c9e7e0a28473
bijit027 <debbijit70@gmail.com>
2023-10-25 14:08:44 +0600

    Removed unnecessary comments from block editor

--- resources/block-editor/blocks/customer-portal/inspectors/CreateTicket/Advanced/ButtonCreateAdvanced.js
+++ resources/block-editor/blocks/customer-portal/inspectors/CreateTicket/Advanced/ButtonCreateAdvanced.js
@@ -6,22 +6,6 @@ import icons from "../../../utils/icons";
 export default function ButtonCreateAdvanced({attributes, setAttributes}) {
     return (
         <PanelBody title={__('All', 'fluent-support')}>
-            {/*<p><strong>{__('Border Width', 'fluent-support')}</strong></p>*/}
-            {/*<RangeControl*/}
-            {/*    value={attributes.createTicketCreateButtonBorderWidth}*/}
-            {/*    onChange={(v) => setAttributes({createTicketCreateButtonBorderWidth: v})}*/}
-            {/*    min={1}*/}
-            {/*    max={5}*/}
-            {/*/>*/}
-
-            {/*<p><strong>{__('Border Radius', 'fluent-support')}</strong></p>*/}
-            {/*<RangeControl*/}
-            {/*    value={attributes.createTicketCreateButtonBorderRadius}*/}
-            {/*    onChange={(v) => setAttributes({createTicketCreateButtonBorderRadius: v})}*/}
-            {/*    min={0}*/}
-            {/*    max={15}*/}
-            {/*/>*/}
-
             <EnhancedColorPicker
                 attributes={attributes}
                 setAttributes={setAttributes}
