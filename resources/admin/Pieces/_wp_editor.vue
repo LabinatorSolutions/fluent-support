2e316173e73dcd24d994eb752a454de1ec4bf0cd
bijit027 <debbijit70@gmail.com>
2024-07-18 11:22:34 +0600

    Added overall ticket response in chatGPT integration

--- resources/admin/Pieces/_wp_editor.vue
+++ resources/admin/Pieces/_wp_editor.vue
@@ -14,8 +14,24 @@
             </div>
 
             <div class="fs_chatGPT_box" v-if="aiResponse">
-                <ChatGPTResponseBox @insert="insertTemplate"/>
+                <el-popover
+                    placement="bottom"
+                    :width="480"
+                    trigger="click"
+                    :visible="showAIResponseBox"
+                >
+                    <template #reference>
+                        <el-button icon="MagicStick" @click="showAIResponseBox = !showAIResponseBox">
+                        </el-button>
+                    </template>
+                    <div class="fs_template_inserter">
+                        <div>
+                            <AIResponseGenerator type="ticketResponse" @insert="insertAIResponse"/>
+                        </div>
+                    </div>
+                </el-popover>
             </div>
+
             <div class="fc_shortcode_box" v-if="showShortcodes" style="padding: 5px;">
                 <el-dropdown type="primary" trigger="click">
                     <el-button size="small" type="primary" style="margin-right: .3em;">
@@ -41,7 +57,7 @@
             v-model="plain_content"
             @click="updateCursorPos"
         ></textarea>
-        <div v-if="showActionBar" :style="actionBarStyle" class="action-bar">
+        <div v-if="showActionBar" :style="actionBarStyle">
             <el-popover
                 placement="bottom"
                 :width="480"
@@ -49,11 +65,17 @@
                 :visible="showChatGPTPromptBox"
             >
                 <template #reference>
-                    <el-button @click="editSelection()" icon="MagicStick" size="small" type="default"></el-button>
+                    <el-button @click="editSelection()" size="small" type="default">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                            <path d="M11.6048 10.6479L9.81458 8.85764C9.57058 8.61364 9.44858 8.49164 9.31692 8.42639C9.0665 8.30229 8.7725 8.30229 8.522 8.42639C8.39042 8.49164 8.26839 8.61364 8.02436 8.85764C7.78032 9.10173 7.6583 9.22373 7.59307 9.35531C7.46898 9.60581 7.46898 9.89981 7.59307 10.1502C7.6583 10.2819 7.78032 10.4039 8.02436 10.6479L9.81458 12.4381M11.6048 10.6479L16.9757 16.0187C17.2197 16.2627 17.3417 16.3847 17.4069 16.5164C17.531 16.7668 17.531 17.0608 17.4069 17.3113C17.3417 17.4429 17.2197 17.5649 16.9757 17.809C16.7316 18.053 16.6096 18.175 16.478 18.2402C16.2275 18.3643 15.9335 18.3643 15.6831 18.2402C15.5514 18.175 15.4294 18.053 15.1854 17.809L9.81458 12.4381M11.6048 10.6479L9.81458 12.4381" stroke="#525866" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M14.1673 1.66669L14.413 2.33052C14.7351 3.201 14.8962 3.63623 15.2137 3.95373C15.5312 4.27123 15.9663 4.43228 16.8368 4.75438L17.5007 5.00002L16.8368 5.24566C15.9663 5.56776 15.5312 5.72882 15.2137 6.04631C14.8962 6.36381 14.7351 6.79904 14.413 7.66952L14.1673 8.33335L13.9217 7.66952C13.5996 6.79905 13.4385 6.36381 13.121 6.04631C12.8035 5.72881 12.3683 5.56776 11.4978 5.24566L10.834 5.00002L11.4978 4.75438C12.3683 4.43228 12.8035 4.27123 13.121 3.95373C13.4385 3.63623 13.5996 3.201 13.9217 2.33052L14.1673 1.66669Z" stroke="#525866" stroke-width="1.25" stroke-linejoin="round"/>
+                            <path d="M5 3.33331L5.18423 3.83119C5.42581 4.48404 5.5466 4.81047 5.78472 5.0486C6.02284 5.28671 6.34927 5.4075 7.00212 5.64908L7.5 5.83331L7.00212 6.01755C6.34927 6.25912 6.02284 6.37991 5.78472 6.61804C5.5466 6.85615 5.42581 7.18259 5.18423 7.83544L5 8.33331L4.81577 7.83544C4.57419 7.18259 4.4534 6.85615 4.21528 6.61804C3.97716 6.37991 3.65073 6.25912 2.99788 6.01755L2.5 5.83331L2.99788 5.64908C3.65073 5.4075 3.97716 5.28671 4.21528 5.04859C4.4534 4.81047 4.57419 4.48404 4.81577 3.83119L5 3.33331Z" stroke="#525866" stroke-width="1.25" stroke-linejoin="round"/>
+                        </svg>
+                    </el-button>
                 </template>
                 <div class="fs_template_inserter">
                     <div>
-                        <ChatGPTPromptBox :selectedText="selectedText" @insert="insertAIResponse"/>
+                        <AIResponseGenerator type="modifyResponse" :selectedText="selectedText" @insert="insertAIResponse"/>
                     </div>
                 </div>
             </el-popover>
@@ -67,8 +89,7 @@ export default {
     name: 'wp_editor',
     components: {
         TemplateInserter: () => true ? import('../Modules/Tickets/_templateInserter') : undefined,
-        ChatGPTResponseBox: () => import('../Modules/Tickets/_ChatGPTResponseBox'),
-        ChatGPTPromptBox: () => import('../Modules/Tickets/_ChatGPTPromptBox'),
+        AIResponseGenerator: () => import('../Modules/Tickets/_AIResponseGenerator'),
     },
     props: {
         editor_id: {
@@ -165,6 +186,7 @@ export default {
             showActionBar: false,
             actionBarStyle: {},
             showChatGPTPromptBox: false,
+            showAIResponseBox: false,
             selectedText: '',
             editorData: {}
         }
@@ -278,6 +300,10 @@ export default {
             }
         },
 
+        overallTicketResponse() {
+            this.showAIResponseBox = !this.showAIResponseBox;
+        },
+
         editSelection() {
             if (!this.editor) {
                 console.error('TinyMCE editor instance or selection not available.');
@@ -296,8 +322,7 @@ export default {
     beforeCreate() {
         if (window.fluentSupportAdmin) {
             this.$options.components.TemplateInserter = require('../Modules/Tickets/_templateInserter').default
-            this.$options.components.ChatGPTResponseBox = require('../Modules/Tickets/_ChatGPTResponseBox').default
-            this.$options.components.ChatGPTPromptBox = require('../Modules/Tickets/_ChatGPTPromptBox').default
+            this.$options.components.AIResponseGenerator = require('../Modules/Tickets/_AIResponseGenerator').default
         }
     },
     mounted() {
