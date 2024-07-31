29cf46a6cd7761443b517d5045a3886e10c699a4
bijit027 <debbijit70@gmail.com>
2024-07-31 14:02:17 +0600

    Added draft response in chatGPT integration

--- app/Services/TransStrings.php
+++ app/Services/TransStrings.php
@@ -173,6 +173,7 @@ class TransStrings
             'Generate Responses with AI'    => __('Generate Responses with AI', 'fluent-support'),
             'Let ChatGPT generate ticket responses to enhance support efficiency.'    => __('Let ChatGPT generate ticket responses to enhance support efficiency.', 'fluent-support'),
             'Some General Prompts'    => __('Some General Prompts', 'fluent-support'),
+            'Draft'    => __('Draft', 'fluent-support'),
             'Import from other Support Tickets Plugins'  => __('Import from other Support Tickets Plugins', 'fluent-support'),
             'If you want to migrate tickets from other ticketing system like'   => __('If you want to migrate tickets from other ticketing system like', 'fluent-support'),
             'Awesome Support'   =>__('Awesome Support', 'fluent-support'),
