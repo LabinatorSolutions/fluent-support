a8924412a772a2b485e51a3085a1c0bb61d24955
bijit027 <debbijit70@gmail.com>
2024-08-14 16:04:57 +0600

    Fix some issues in email verification

--- app/Services/Helper.php
+++ app/Services/Helper.php
@@ -1116,6 +1116,41 @@ class Helper
         return $ipAddress;
     }
 
+    public static function isCfIp($ip = '')
+    {
+        if (!$ip) {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        $cloudflareIPRanges = array(
+            '103.21.244.0/22',
+            '103.22.200.0/22',
+            '103.31.4.0/22',
+            '104.16.0.0/13',
+            '104.24.0.0/14',
+            '108.162.192.0/18',
+            '131.0.72.0/22',
+            '141.101.64.0/18',
+            '162.158.0.0/15',
+            '172.64.0.0/13',
+            '173.245.48.0/20',
+            '188.114.96.0/20',
+            '190.93.240.0/20',
+            '197.234.240.0/22',
+            '198.41.128.0/17'
+        );
+        $validCFRequest = false;
+        //Make sure that the request came via Cloudflare.
+        foreach ($cloudflareIPRanges as $range) {
+            //Use the ip_in_range function from Joomla.
+            if (self::ipInRange($ip, $range)) {
+                //IP is valid. Belongs to Cloudflare.
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public static function loadView($template, $data)
     {
         extract($data, EXTR_OVERWRITE);
