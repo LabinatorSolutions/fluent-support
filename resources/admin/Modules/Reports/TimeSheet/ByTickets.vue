b20487d3161fa2fb5ffb01197721d3499b0ebafe
bijit027 <debbijit70@gmail.com>
2024-11-25 11:15:51 +0600

    Refactor: Mailbox not load issue and style issue

--- resources/admin/Modules/Reports/TimeSheet/ByTickets.vue
+++ resources/admin/Modules/Reports/TimeSheet/ByTickets.vue
@@ -1,84 +1,88 @@
 <template>
-    <div class="fs_time_sheet_box_header">
-        <div class="fs_time_sheet_box_head">
-            {{"Time Sheet Report By Tickets"}}
-        </div>
-        <div class="fs_time_sheet_box_actions">
-            <el-select
-                v-model="selectedMailBoxes"
-                @change="fetchReportsByTickets"
-                placeholder="Select a Mailbox"
-                clearable
-                class="fs_time_sheet_select"
-            >
-                <el-option
-                    v-for="mailBox in allMailBoxes"
-                    :key="mailBox.key"
-                    :label="mailBox.label"
-                    :value="mailBox.key"
-                />
-            </el-select>
-            <div class="fs_time_sheet_export">
-                <el-button type="default" @click="openSettings = true">
-                    <el-icon>
-                        <Download/>
-                    </el-icon>
-                    {{ translate('Export') }}
-                </el-button>
+    <div class="fs_time_sheet"  v-if="isLoaded">
+        <div class="fs_time_sheet_box_header">
+            <div class="fs_time_sheet_box_head">
+                {{"Time Sheet Report By Tickets"}}
+            </div>
+            <div class="fs_time_sheet_box_actions">
+                <el-select
+                    v-model="selectedMailBoxes"
+                    @change="fetchReportsByTickets"
+                    placeholder="Select a Mailbox"
+                    clearable
+                    class="fs_time_sheet_select"
+                >
+                    <el-option
+                        v-for="mailBox in allMailBoxes"
+                        :key="mailBox.key"
+                        :label="mailBox.label"
+                        :value="mailBox.key"
+                    />
+                </el-select>
+                <div class="fs_time_sheet_export">
+                    <el-button type="default" @click="openSettings = true">
+                        <el-icon>
+                            <Download/>
+                        </el-icon>
+                        {{ translate('Export') }}
+                    </el-button>
+                </div>
             </div>
         </div>
+        <div class="fs_time_sheet_box_actions">
+            <el-table :data="tasks" style="width: 100%">
+                <el-table-column fixed="left" prop="ticket" label="Ticket" min-width="120">
+                    <template #default="{ row }">
+                        <strong>{{ row.ticket.title }}</strong>
+                    </template>
+                </el-table-column>
+                <el-table-column :min-width="100" v-for="date in dateLabels" :key="date" :prop="date" :label="date" min-width="100">
+                    <template #header="{ column }">
+                        <span>{{ smartDate(column.label) }}</span>
+                    </template>
+                    <template #default="{ row }">
+                        <TicketDateSheetPop :ticket_id="row.ticket.id" :date="date" :timeSheets="timeSheets" />
+                    </template>
+                </el-table-column>
+                <el-table-column label="Total" width="160" fixed="right">
+                    <template #header="{ column }">
+                        <span>Total ({{ formatMinutes(totalMinutes) }})</span>
+                    </template>
+                    <template #default="{ row }">
+                        <strong>{{ getTicketTotal(row.ticket) }}</strong>
+                    </template>
+                </el-table-column>
+
+                <Teleport to="body">
+                    <modal :show="openSettings" :title="translate('Include Customers in Time Sheet')"
+                           @close="openSettings = false">
+                        <template #body>
+                            <div class="fs_summary_settings">
+                                <el-row :gutter="20">
+                                    <el-col :span="18">
+                                        <span> {{ translate("If you don't select any customer then all the customers report will be displayed here otherwise it will show only selected agents report.") }}</span>
+                                    </el-col>
+                                </el-row>
+
+                                <el-transfer
+                                    v-model="includeOrExcludeMailBoxes"
+                                    :data="allMailBoxes"
+                                    :titles="['Available Mail Boxes', 'Selected Mail Boxes']"
+                                    filterable
+                                />
+                            </div>
+                        </template>
+                        <template #footer>
+                            <el-button @click="openSettings = false">{{ translate('Cancel') }}</el-button>
+                            <el-button type="primary" @click="exportReport">{{ translate('Export') }}</el-button>
+                        </template>
+                    </modal>
+                </Teleport>
+            </el-table>
+        </div>
     </div>
-    <div class="fs_time_sheet_box_actions">
-        <el-table v-if="isLoaded" :data="tasks" style="width: 100%">
-            <el-table-column fixed="left" prop="ticket" label="Ticket" min-width="120">
-                <template #default="{ row }">
-                    <strong>{{ row.ticket.title }}</strong>
-                </template>
-            </el-table-column>
-            <el-table-column :min-width="100" v-for="date in dateLabels" :key="date" :prop="date" :label="date" min-width="100">
-                <template #header="{ column }">
-                    <span>{{ smartDate(column.label) }}</span>
-                </template>
-                <template #default="{ row }">
-                    <TicketDateSheetPop :ticket_id="row.ticket.id" :date="date" :timeSheets="timeSheets" />
-                </template>
-            </el-table-column>
-            <el-table-column label="Total" width="160" fixed="right">
-                <template #header="{ column }">
-                    <span>Total ({{ formatMinutes(totalMinutes) }})</span>
-                </template>
-                <template #default="{ row }">
-                    <strong>{{ getTicketTotal(row.ticket) }}</strong>
-                </template>
-            </el-table-column>
-
-            <Teleport to="body">
-                <modal :show="openSettings" :title="translate('Include Customers in Time Sheet')"
-                       @close="openSettings = false">
-                    <template #body>
-                        <div class="fs_summary_settings">
-                            <el-row :gutter="20">
-                                <el-col :span="18">
-                                    <span> {{ translate("If you don't select any customer then all the customers report will be displayed here otherwise it will show only selected agents report.") }}</span>
-                                </el-col>
-                            </el-row>
-
-                            <el-transfer
-                                v-model="includeOrExcludeMailBoxes"
-                                :data="allMailBoxes"
-                                :titles="['Available Mail Boxes', 'Selected Mail Boxes']"
-                                filterable
-                            />
-                        </div>
-                    </template>
-                    <template #footer>
-                        <el-button @click="openSettings = false">{{ translate('Cancel') }}</el-button>
-                        <el-button type="primary" @click="exportReport">{{ translate('Export') }}</el-button>
-                    </template>
-                </modal>
-            </Teleport>
-        </el-table>
-        <el-skeleton :animated="true" :rows="10" v-else />
+    <div class="fs_time_sheet_skeleton"  v-else>
+        <el-skeleton :animated="true" :rows="10"  />
     </div>
 </template>
 
@@ -101,7 +105,6 @@ export default {
     setup(props, { emit }) {
         const { get, translate, handleError, smartDate } = useFluentHelper();
         const tasks = ref([]);
-        const mailBoxes = ref([]);
         const allMailBoxes = ref([]);
         const totalMinutes = ref(0);
         const timeSheets = ref({});
@@ -120,17 +123,14 @@ export default {
             try {
                 const response = await get('reports/timesheet/by-tickets', params);
                 tasks.value = response.tasks;
-                mailBoxes.value = response.mail_boxes;
                 timeSheets.value = response.time_sheets;
                 dateLabels.value = response.date_labels;
                 totalMinutes.value = response.total_minutes;
 
-                if (!params.mailbox_id) {
-                    allMailBoxes.value = response.mail_boxes.map(mailBox => ({
-                        key: mailBox.id,
-                        label: mailBox.name
-                    }));
-                }
+                allMailBoxes.value = response.mail_boxes.map(mailBox => ({
+                    key: mailBox.id,
+                    label: mailBox.name
+                }));
 
             } catch (error) {
                 handleError(error)
@@ -169,7 +169,6 @@ export default {
             formatMinutes,
             smartDate,
             openSettings,
-            mailBoxes,
             allMailBoxes,
             selectedMailBoxes,
             includeOrExcludeMailBoxes,
