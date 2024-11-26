a201d0584ba50eacf19740f1517890b8fd18de0e
bijit027 <debbijit70@gmail.com>
2024-11-26 16:12:49 +0600

    Update: Giving a link to agent name in ticket pop over in agent time sheet report

--- resources/admin/Modules/Reports/TimeSheet/_TicketDateSheetPop.vue
+++ resources/admin/Modules/Reports/TimeSheet/_TicketDateSheetPop.vue
@@ -16,7 +16,13 @@
                 <template #default="{ row }">
                     <div class="fs_time_sheet_person">
                         <el-avatar :src="row.agent?.photo" size="small"></el-avatar>
-                        <span>{{ row.agent?.full_name }}</span>
+                        <a
+                            :href="`/wp-admin/user-edit.php?user_id=${row.agent.id}`"
+                            target="_blank"
+                            style="text-decoration: none; margin-left: 8px;">
+                            <strong> {{ row.agent.full_name }} </strong>
+                        </a>
+
                     </div>
                 </template>
             </el-table-column>
