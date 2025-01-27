9cc83efcfe03fb77f0b97af961209e7d3fdfff6c
bijit027 <debbijit70@gmail.com>
2025-01-27 14:01:55 +0600

    Added: Tickets vue header part name and action button ongoing

---
+++ resources/custom_customer_portal/components/Tickets.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="fs_ticket_wrapper">
+        <div class="fs_tickets_container">
+            <div class="fs_tickets_header">
+                <label>All Tickets</label>
+                <el-button class="fs_create_ticket_btn" @click="$router.push({ name: 'create_ticket' })">
+                    + Create a New Ticket
+                </el-button>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+import {debounce} from 'lodash'
+import {List} from '@element-plus/icons-vue'
+
+export default {
+    name: 'TicketsList',
+    components: {
+        List
+    },
+    data() {
+        return {
+            tickets: [],
+            filterType: 'all',
+            loading: true,
+            search: '',
+            filters: {
+                product_id: ''
+            },
+            pagination: {
+                per_page: 10,
+                current_page: 1,
+                total: 0
+            },
+            sortingColumns: [
+                {
+                    label: this.$t('Ticket ID'),
+                    value: 'id'
+                },
+                {
+                    label: this.$t('Title'),
+                    value: 'title'
+                },
+                {
+                    label: this.$t('Created at'),
+                    value: 'created_at'
+                },
+            ],
+            sorting: {
+                sort_by: 'id',
+                sort_type: 'desc'
+            }
+        }
+    },
+}
+</script>
+
+
+
+
+
+
+
