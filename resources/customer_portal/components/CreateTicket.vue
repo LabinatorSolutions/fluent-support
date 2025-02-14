c3cc38f8f22043fcf26a77357ef062e43ac8277f
bijit027 <debbijit70@gmail.com>
2025-02-14 10:59:17 +0600

    Added: Back to all tickets button in create ticket page

--- resources/customer_portal/components/CreateTicket.vue
+++ resources/customer_portal/components/CreateTicket.vue
@@ -1,177 +1,86 @@
 <template>
-    <div class="fs_all_tickets">
-        <div class="fs_tk_actions fs_tk_header fs_tk_create_head">
-            <div class="fs_tk_left">
-                <h3>{{$t('submit_heading')}}</h3>
-            </div>
-            <div class="fs_tk_right">
-                <el-button class="fs_view_all_button" @click="$router.push({ name: 'dashboard' })" size="small" type="info">{{$t('View All')}}</el-button>
-            </div>
+    <div class="fs_support_ticket_container">
+        <!-- Back Button -->
+        <div class="fs_back_nav">
+            <el-button link class="fs_back_button" @click="$router.push({ name: 'dashboard' })">
+                <svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M2.20437 4.9992L5.91687 8.7117L4.85637 9.7722L0.083374 4.9992L4.85637 0.226196L5.91687 1.2867L2.20437 4.9992Z"
+                        fill="#0E121B"/>
+                </svg>
+                <span>Back to All Tickets</span>
+            </el-button>
         </div>
-        <div style=" padding: 20px;" class="fs_tk_body">
-            <el-form :data="ticket" label-position="top">
-                <el-form-item class="fs_input_form fs_input_label" :label="$t('subject')">
-                    <el-input :placeholder="$t('subject_placeholder')" type="text" v-model="ticket.title"></el-input>
-                    <error :error="errors.get('title')"/>
-                </el-form-item>
 
-                <div class="fs_tk_suggestions" v-if="fetchingSuggestions || suggestions.length">
-                    <p v-if="fetchingSuggestions">{{$t('suggestion_loading')}}</p>
-
-                    <template v-else>
-                        <p class="fs_tk_suggestion_articles">
-                            <b>{{ $t('articles_heading') }}</b>
-                        </p>
-
-                        <p v-for="suggestion in suggestions">
-                            <a :href="suggestion.link" target="_blank" class="fs_tk_suggestion">
-                                {{ suggestion.title }}
-                            </a>
-                        </p>
-                    </template>
-                </div>
-
-                <el-form-item class="fs_tk_suggestions" >
-                    <template #label>
-                        <label class="fs_input_label"> {{$t('ticket_details')}}</label>
-                    </template>
-                    <wp-editor :height="150" :media-buttons="false" :is_direct_paste="true" v-model="ticket.content"/>
-                    <p class="fs_tk_help">{{$t('details_help')}}</p>
-                    <error :error="errors.get('content')"/>
-                </el-form-item>
-
-                <attachment-form v-if="appVars.has_file_upload" :ticket="ticket" :attachments="attachments" />
-
-                <div v-if="products.length || Object.keys(priorities).length" class="fs_tk_row">
-                    <div v-if="products.length" class="fs_tk_col">
-                        <el-form-item class="fs_ticket_product fs_input_label" :label="$t('product_services')" :required="isProductFieldRequired">
-                            <el-select clearable v-model="ticket.product_id" :placeholder="$t('service_placeholder')">
-                                <el-option v-for="product in products" :key="product.id" :value="product.id" :label="product.title"></el-option>
-                            </el-select>
-                            <error :error="errors.get('product_id')"/>
-                        </el-form-item>
-                    </div>
-                    <div v-if="Object.keys(priorities).length" class="fs_tk_col">
-                        <el-form-item class="fs_ticket_priority fs_input_label" :label="$t('priority')">
-                            <el-select clearable v-model="ticket.client_priority" :placeholder="$t('priority_placeholder')">
-                                <el-option v-for="(priority,priorityKey) in priorities" :key="priorityKey" :value="priorityKey" :label="priority"></el-option>
-                            </el-select>
-                        </el-form-item>
-                        <error :error="errors.get('client_priority')"/>
-                    </div>
-                </div>
-
-                <custom-fields-form :ticket="ticket" :custom_data="custom_data" :exceptions="exceptions" />
-
-                <el-form-item>
-                    <el-button class="fs_create_button" @click="create()" :disabled="creating" v-loading="creating" type="success">{{$t('btn_text')}}</el-button>
-                </el-form-item>
-
-            </el-form>
-        </div>
     </div>
 </template>
 
-<script type="text/babel">
-import WpEditor from '../../admin/Pieces/_wp_editor';
-import Error from '../../admin/Pieces/Error';
-import Errors from '../../admin/Bits/Errors';
-import AttachmentForm from "../../admin/Modules/Tickets/_AttachmentForm";
+<script>
+import {defineComponent} from 'vue'
+import {ArrowLeft, List, UploadFilled} from '@element-plus/icons-vue'
+import WpEditor from '../../admin/Pieces/_wp_editor'
+import Error from '../../admin/Pieces/Error'
+import Errors from '../../admin/Bits/Errors'
 import CustomFieldsForm from "./_CustomFieldForm";
 import {debounce} from "lodash";
 
-
-export default {
+export default defineComponent({
     name: 'CreateTicket',
+
     components: {
         WpEditor,
         Error,
-        AttachmentForm,
-        CustomFieldsForm
+        ArrowLeft,
+        UploadFilled,
+        CustomFieldsForm,
+        List,
+        AttachmentForm
     },
+
     data() {
         return {
             errors: new Errors(),
             creating: false,
+            editorType: 'paragraph',
+            editorMode: 'Visual',
+            custom_data: {},
             ticket: {
                 title: '',
                 content: '',
                 product_id: '',
                 client_priority: 'normal',
+                name: '',
                 mailbox_id: this.appVars.mailbox_id ? this.appVars.mailbox_id : null,
             },
-            custom_data: {},
-            attachments: [],
             products: this.appVars.support_products,
-            isProductFieldRequired: !!this.appVars.product_field_required,
             priorities: this.appVars.customer_ticket_priorities,
+            attachments: [],
             suggestions: [],
             fetchingSuggestions: false,
+            shouldShowSuggestions: false,
         }
     },
-    computed: {
-        exceptions() {
-            return this.errors.errors;
-        }
-    },
-    watch: {
-        'ticket.title': function (newVal, oldVal) {
 
-            if(!this.appVars.has_doc_integration) {
-                return;
-            }
-
-            if (this.hasQueryOrSpace(newVal)) {
-                this.fetchingSuggestions = true;
-                this.debouncedGetSuggestions();
-            } else {
-                this.fetchingSuggestions = false;
-                this.suggestions = [];
-            }
-        }
-    },
-    created () {
-        this.debouncedGetSuggestions = debounce(this.getSuggestions, 500);
-    },
     methods: {
         create() {
-            this.errors.clear();
-            this.creating = true;
+            this.errors.clear()
+            this.creating = true
             this.$post('tickets', {
                 ...this.ticket,
                 attachments: this.attachments,
                 custom_data: this.custom_data
             })
-            .then(response => {
-                this.$router.push({ name: 'view_ticket', params: { ticket_id: response.ticket.id } });
-            })
-            .catch((errors) => {
-                this.errors.record(errors);
-            })
-            .always(() => {
-                this.creating = false;
-            });
-        },
-
-        getSuggestions() {
-            this.$get('search-doc', {
-                search: this.ticket.title
-            })
                 .then(response => {
-                    this.suggestions = response;
+                    this.$router.push({name: 'view_ticket', params: {ticket_id: response.ticket.id}});
                 })
                 .catch((errors) => {
-                    console.log(errors);
-                    // this.errors.record(errors);
+                    this.errors.record(errors)
+                    this.creating = false;
                 })
-                .always(() => {
-                    this.fetchingSuggestions = false;
-                });
+                .finally(() => {
+                    this.creating = false
+                })
         },
-
-        hasQueryOrSpace(search) {
-            return search && (search.length >= 5 || (search.split(' ').length > 1));
-        }
     }
-}
+})
 </script>
