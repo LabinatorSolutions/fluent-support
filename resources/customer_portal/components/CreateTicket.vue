93b0ac97f5340a92bcd998d4544d5407b90476ad
bijit027 <debbijit70@gmail.com>
2025-02-14 11:05:49 +0600

    Added: Form element

--- resources/customer_portal/components/CreateTicket.vue
+++ resources/customer_portal/components/CreateTicket.vue
@@ -12,6 +12,96 @@
             </el-button>
         </div>
 
+        <div class="fs_ticket_form_container">
+            <div class="fs_ticket_header">
+                <label>Submit a Support Ticket</label>
+            </div>
+            <el-form :model="ticket" label-position="top" class="fs_ticket_form">
+                <el-form-item label="Subject" class="fs_input_wrapper">
+                    <el-input
+                        size="default"
+                        v-model="ticket.title"
+                        :class="{ 'error': errors.get('title') }"
+                        placeholder="What's this support ticket about?"
+                    />
+
+                    <div v-if="errors.get('title')" class="error-message">
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M8 14C4.6862 14 2 11.3138 2 8C2 4.6862 4.6862 2 8 2C11.3138 2 14 4.6862 14 8C14 11.3138 11.3138 14 8 14ZM7.4 7.4V11H8.6V7.4H7.4ZM7.4 5V6.2H8.6V5H7.4Z"
+                                fill="#FB3748"/>
+                        </svg>
+                        <error :error="errors.get('title')"/>
+                    </div>
+
+                </el-form-item>
+
+                <div v-if="products.length || Object.keys(priorities).length" class="fs_tk_row">
+                    <div v-if="products.length" class="fs_tk_col">
+                        <el-form-item class="fs_ticket_product fs_input_label" :label="$t('product_services')"
+                                      :required="isProductFieldRequired">
+                            <el-select clearable v-model="ticket.product_id"
+                                       :class="{ 'error': errors.get('product_id') }"
+                                       size="large"
+                                       :placeholder="$t('service_placeholder')">
+                                <el-option v-for="product in products" :key="product.id" :value="product.id"
+                                           :label="product.title"></el-option>
+                            </el-select>
+
+                            <div v-if="errors.get('product_id')" class="error-message">
+                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M8 14C4.6862 14 2 11.3138 2 8C2 4.6862 4.6862 2 8 2C11.3138 2 14 4.6862 14 8C14 11.3138 11.3138 14 8 14ZM7.4 7.4V11H8.6V7.4H7.4ZM7.4 5V6.2H8.6V5H7.4Z"
+                                        fill="#FB3748"/>
+                                </svg>
+                                <error :error="errors.get('product_id')"/>
+                            </div>
+
+
+                        </el-form-item>
+                    </div>
+                </div>
+
+                <!-- Priority -->
+                <el-form-item label="Priority" v-if="Object.keys(priorities).length">
+                    <el-select
+                        v-model="ticket.client_priority"
+                        placeholder="Normal"
+                        size="large"
+                        :class="{ 'error': errors.get('product_id') }"
+                    >
+                        <el-option
+                            v-for="(priority, key) in priorities"
+                            :key="key"
+                            :label="priority"
+                            :value="key"
+                        />
+                    </el-select>
+
+                    <div v-if="errors.get('client_priority')" class="error-message">
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M8 14C4.6862 14 2 11.3138 2 8C2 4.6862 4.6862 2 8 2C11.3138 2 14 4.6862 14 8C14 11.3138 11.3138 14 8 14ZM7.4 7.4V11H8.6V7.4H7.4ZM7.4 5V6.2H8.6V5H7.4Z"
+                                fill="#FB3748"/>
+                        </svg>
+                        <error :error="errors.get('client_priority')"/>
+                    </div>
+                </el-form-item>
+
+                <!-- Submit Button -->
+                <el-form-item class="fs_submit_button_container">
+                    <el-button
+                        type="primary"
+                        :loading="creating"
+                        @click="create"
+                        class="fs_create_ticket_button"
+                    >
+                        Create Ticket
+                    </el-button>
+                </el-form-item>
+            </el-form>
+        </div>
     </div>
 </template>
 
@@ -34,7 +124,6 @@ export default defineComponent({
         UploadFilled,
         CustomFieldsForm,
         List,
-        AttachmentForm
     },
 
     data() {
@@ -81,6 +170,7 @@ export default defineComponent({
                     this.creating = false
                 })
         },
+
     }
 })
 </script>
