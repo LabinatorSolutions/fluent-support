521f228503631a874a32ad379935bd1c19026468
bijit027 <debbijit70@gmail.com>
2025-03-13 12:22:42 +0600

    Update: Block editor attributes and styles.

--- app/Services/Blocks/BlockAttributes.php
+++ app/Services/Blocks/BlockAttributes.php
@@ -10,105 +10,29 @@ class BlockAttributes
     public static function CustomerPortalAttributes()
     {
         return [
-            'title' => [
-                'type' => 'string',
-                'default' => 'All Tickets',
-            ],
-            'perPage' => [
-                'type' => 'string',
-                'default' => '10',
-            ],
-            'selectedFilter' => [
-                'type' => 'string',
-                'default' => 'all',
-            ],
-            'selectedProduct' => [
-                'type' => 'string',
-                'default' => 'all',
-            ],
-            'searchPlaceholder' => [
-                'type' => 'string',
-                'default' => 'Search...',
-            ],
-            'showPagination' => [
-                'type' => 'boolean',
-                'default' => true,
-            ],
-            'showFilters' => [
-                'type' => 'boolean',
-                'default' => true,
-            ],
-            'noTicketsMessage' => [
-                'type' => 'string',
-                'default' => 'No tickets found',
-            ],
-            'loadingMessage' => [
-                'type' => 'string',
-                'default' => 'Loading tickets...',
-            ],
-            'createButtonText' => [
-                'type' => 'string',
-                'default' => 'Create a New Ticket',
-            ],
-            'allTicketsBorderRadius' => [
+            'containerBorderRadius' => [
                 'type' => 'number',
                 'default' => 16,
             ],
-            'allTicketsHeaderBackgroundColor' => [
-                'type' => 'string',
-                'default' => '#FFFFFF',
-            ],
-            'createTicketButtonBorderRadius' => [
-                'type' => 'number',
-                'default' => 16,
-            ],
-            'buttonBorderRadius' => [
-                'type' => 'number',
-                'default' => 8,
-            ],
-            'createTicketButtonBackgroundColor' => [
+            'primaryButtonBgColor' => [
                 'type' => 'string',
                 'default' => 'rgba(14, 18, 27, 1)',
             ],
-            'createTicketButtonTextColor' => [
+            'primaryButtonTextColor' => [
                 'type' => 'string',
                 'default' => 'rgba(255, 255, 255, 1)',
             ],
-            'allTicketsFilterBorderRadius' => [
-                'type' => 'number',
-                'default' => 8,
-            ],
-            'allTicketsHeaderTextColor' => [
+            'secondaryButtonBgColor' => [
                 'type' => 'string',
-                'default' => 'rgba(14, 18, 27, 1)',
+                'default' => 'rgba(255, 255, 255, 1)',
             ],
-            'allTicketsPrimaryTextColor' => [
+            'secondaryButtonTextColor' => [
                 'type' => 'string',
-                'default' => '#000000',
+                'default' => '#18181B',
             ],
-            'allTicketsDescriptionColor' => [
-                'type' => 'string',
-                'default' => 'rgba(82, 88, 102, 1)',
-            ],
-            'allTicketsTableHeaderBorderRadius' => [
+            'ticketInputBorderRadius' => [
                 'type' => 'number',
-                'default' => 8,
-            ],
-            'allTicketsFooterTextColor' => [
-                'type' => 'string',
-                'default' => 'rgba(82, 88, 102, 1)',
-            ],
-            'allTicketsFooterButtonBorderRadius' => [
-                'type' => 'number',
-                'default' => 8,
-            ],
-            'allTicketsLogoutButtonVisibility' => [
-                'type' => 'boolean',
-                'default' => true,
-            ],
-            'businessBoxId' => [
-                'type' => 'number',
-                'default' => '',
+                'default' => 10,
             ],
         ];
     }
--- app/Services/Blocks/BlockHelper.php
+++ app/Services/Blocks/BlockHelper.php
@@ -21,36 +21,35 @@ class BlockHelper
         ?>
         <style type="text/css">
             #fluent_support_client_app {
-                .fs_tickets_container {
-                    border-radius: <?php echo isset($attributes['allTicketsBorderRadius']) ? $attributes['allTicketsBorderRadius'] : 16; ?>px;
+                .fs_tickets_container,
+                .fs_ticket_form_container {
+                    border-radius: <?php echo $attributes['containerBorderRadius'] ?? 16; ?>px;
                 }
 
-                .fs_ticket_wrapper .fs_tickets_container .fs_tickets_header .fs_create_ticket_btn {
-                    border-radius: <?php echo isset($attributes['createTicketButtonBorderRadius']) ? $attributes['createTicketButtonBorderRadius'] : 8; ?>px;
-                    background-color: <?php echo isset($attributes['createTicketButtonBackgroundColor']) ? $attributes['createTicketButtonBackgroundColor'] : '#409eff'; ?>;
-                    color: <?php echo isset($attributes['createTicketButtonTextColor']) ? $attributes['createTicketButtonTextColor'] : '#ffffff'; ?>;
+                .fs_create_ticket_btn,
+                .fs_create_ticket_button,
+                .fs_reply_btn {
+                    background-color: <?php echo $attributes['primaryButtonBgColor'] ?? 'rgba(14, 18, 27, 1)'; ?>;
+                    color: <?php echo $attributes['primaryButtonTextColor'] ?? 'rgba(255, 255, 255, 1)'; ?>;
                 }
 
-                .fs_button_groups,
-                .fs_product_filter .el-select__wrapper,
-                .fs_sorting .el-button,
-                .fs_search_filter .el-input__wrapper {
-                    border-radius: <?php echo isset($attributes['allTicketsFilterBorderRadius']) ? $attributes['allTicketsFilterBorderRadius'] : 8; ?>px;
+                .fs_ticket_refresh_btn,
+                .fs-upload-button {
+                    background-color: <?php echo $attributes['secondaryButtonBgColor'] ?? 'rgba(255, 255, 255, 1)'; ?>;
+                    color: <?php echo $attributes['secondaryButtonTextColor'] ?? '#18181B'; ?>;
+                }
+                .fs_close_ticket {
+                    background-color: <?php echo $attributes['secondaryButtonBgColor'] ?? '#F5F7FA'; ?>;
+                    color: <?php echo $attributes['secondaryButtonTextColor'] ?? '#525866'; ?>;
+                    opacity: 0.8;
+                    filter: brightness(0.8);
                 }
 
-                .fs_tickets_table .el-table__header thead th:first-child {
-                    border-top-left-radius: <?php echo isset($attributes['allTicketsTableHeaderBorderRadius']) ? $attributes['allTicketsTableHeaderBorderRadius'] : 8; ?>px;
-                    border-bottom-left-radius: <?php echo isset($attributes['allTicketsTableHeaderBorderRadius']) ? $attributes['allTicketsTableHeaderBorderRadius'] : 8; ?>px;
-                }
-
-                .fs_tickets_table .el-table__header thead th:last-child {
-                    border-top-right-radius: <?php echo isset($attributes['allTicketsTableHeaderBorderRadius']) ? $attributes['allTicketsTableHeaderBorderRadius'] : 8; ?>px;
-                    border-bottom-right-radius: <?php echo isset($attributes['allTicketsTableHeaderBorderRadius']) ? $attributes['allTicketsTableHeaderBorderRadius'] : 8; ?>px;
-                }
-
-                .fs_pagination_container .el-select__wrapper,
-                .fs_pagination_container .el-pagination .el-pager li {
-                    border-radius: <?php echo isset($attributes['allTicketsFooterButtonBorderRadius']) ? $attributes['allTicketsFooterButtonBorderRadius'] : 8; ?>px;
+                .fs_client_portal
+                .fs_create_ticket_container .el-input__wrapper,
+                .fs_create_ticket_container .el-select__wrapper,
+                .fs_custom_fields_wrap .el-select__wrapper {
+                    border-radius: <?php echo $attributes['ticketInputBorderRadius'] ?? 10; ?>px;
                 }
             }
 
