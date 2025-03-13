9c239229b4a02465e05d3f19e0e1ed6c0a816f34
bijit027 <debbijit70@gmail.com>
2025-03-13 11:52:07 +0600

    Added: Separate inspector for block editor create ticket

---
+++ resources/block-editor/blocks/customerPortal/CreateTicket/InspectorSettings.js
@@ -0,0 +1,96 @@
+import React from 'react';
+const { InspectorControls, ColorPalette } = wp.blockEditor;
+const {
+    PanelBody,
+    RangeControl
+} = wp.components;
+const { __ } = wp.i18n;
+
+const generateStyles = (attributes) => {
+    return {
+        blockStyles: {
+            borderRadius: `${attributes.containerBorderRadius || 0}px`,
+        },
+        primaryButtonStyles: {
+            backgroundColor: attributes.primaryButtonBgColor || '#007cba',
+            color: attributes.primaryButtonTextColor || '#ffffff',
+        },
+        secondaryButtonStyles: {
+            backgroundColor: attributes.secondaryButtonBgColor || '#007cba',
+            color: attributes.secondaryButtonTextColor || '#ffffff',
+        },
+        inputFieldStyle: {
+            borderRadius: `${attributes.ticketInputBorderRadius || 0}px`,
+        }
+    };
+};
+
+const CreateTicketInspectorControls = ({ attributes, setAttributes }) => {
+    return (
+        <InspectorControls>
+            <PanelBody title={__('General Style Settings')} initialOpen={true}>
+                <RangeControl
+                    label={__('Border Radius')}
+                    value={attributes.containerBorderRadius || 0}
+                    onChange={value => setAttributes({ containerBorderRadius: value })}
+                    min={0}
+                    max={20}
+                />
+                <PanelBody title={__('Primary Button Style')}>
+                    <div className="components-base-control">
+                        <label className="components-base-control__label">
+                            {__('Primary Button Text Color')}
+                        </label>
+                        <ColorPalette
+                            value={attributes.primaryButtonTextColor}
+                            onChange={value => setAttributes({ primaryButtonTextColor: value })}
+                        />
+                    </div>
+                    <div className="components-base-control">
+                        <label className="components-base-control__label">
+                            {__('Primary Button Background Color')}
+                        </label>
+                        <ColorPalette
+                            value={attributes.primaryButtonBgColor}
+                            onChange={value => setAttributes({ primaryButtonBgColor: value })}
+                        />
+                    </div>
+                </PanelBody>
+                <PanelBody title={__('Secondary Button Style')}>
+                    <div className="components-base-control">
+                        <label className="components-base-control__label">
+                            {__('Secondary Button Background Color')}
+                        </label>
+                        <ColorPalette
+                            value={attributes.secondaryButtonBgColor}
+                            onChange={value => setAttributes({secondaryButtonBgColor: value})}
+                        />
+                    </div>
+                    <div className="components-base-control">
+                        <label className="components-base-control__label">
+                            {__('Secondary Button Text Color')}
+                        </label>
+                        <ColorPalette
+                            value={attributes.secondaryButtonTextColor}
+                            onChange={value => setAttributes({secondaryButtonTextColor: value})}
+                        />
+                    </div>
+
+                </PanelBody>
+            </PanelBody>
+            <PanelBody title={__('Page wise Style Settings')} initialOpen={true}>
+                <RangeControl
+                    label={__('Input Field Border Radius')}
+                    value={attributes.ticketInputBorderRadius || 0}
+                    onChange={value => setAttributes({ticketInputBorderRadius: value})}
+                    min={0}
+                    max={20}
+                />
+            </PanelBody>
+
+        </InspectorControls>
+    );
+};
+
+export { generateStyles };
+export default CreateTicketInspectorControls;
