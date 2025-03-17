96ac26f4583c6b72adb377293c8c5fd4c989177b
bijit027 <debbijit70@gmail.com>
2025-03-17 11:11:14 +0600

    Update: Block editor code

--- webpack.mix.js
+++ webpack.mix.js
@@ -49,4 +49,4 @@ mix
     .copy('resources/images', 'assets/images')
     .copy('resources/libs', 'assets/libs');
 mix.js('resources/block-editor/index.js', 'assets/block-editor/js/fst_block.js').react();
-mix.js('resources/block-editor/blocks/customerPortal/index.js', 'assets/block-editor/js/AllTicketsIndex.js').react();
+mix.js('resources/block-editor/blocks/customer-portal/index.js', 'assets/block-editor/js/fs_block.js').react();
