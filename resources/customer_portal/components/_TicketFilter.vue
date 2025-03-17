17847eae64f61c9d2f81dc9cb8f1dc9fbb1d3b5e
sabbir345 <sabbircse94@gmail.com>
2025-03-17 13:18:43 +0600

    Fix: responsive issue

--- resources/customer_portal/components/_TicketFilter.vue
+++ resources/customer_portal/components/_TicketFilter.vue
@@ -14,60 +14,62 @@
                     </button>
                 </div>
             </div>
-            <div class="fs_product_filter">
-                <el-select
-                    v-model="filters.product_id"
-                    placeholder="All Products"
-                    clearable
-                    @change="fetchTickets"
-                    style="width: 180px"
-                >
-                    <el-option
-                        v-for="product in appVars.support_products"
-                        :key="product.id"
-                        :value="product.id"
-                        :label="product.title"
-                    />
-                </el-select>
-            </div>
-            <div class="fs_sorting">
-                <el-popover trigger="click" placement="bottom-start" width="240" popper-class="fs_popover">
-                    <div class="fs_popover_item">
-                        <label class="fs_popover_title">{{ $t('Sort By') }}</label>
-                        <div class="fs_radio_blocks">
-                            <el-radio-group @change="fetchTickets" v-model="sorting.sort_by">
-                                <el-radio v-for="column in sortingColumns" :key="column.value" :label="column.value">
-                                    {{ column.label }}
-                                </el-radio>
+            <div class="fs_product_filter_head">
+                <div class="fs_product_filter">
+                    <el-select
+                        v-model="filters.product_id"
+                        placeholder="All Products"
+                        clearable
+                        @change="fetchTickets"
+                        style="width: 180px"
+                    >
+                        <el-option
+                            v-for="product in appVars.support_products"
+                            :key="product.id"
+                            :value="product.id"
+                            :label="product.title"
+                        />
+                    </el-select>
+                </div>
+                <div class="fs_sorting">
+                    <el-popover trigger="click" placement="bottom-start" width="240" popper-class="fs_popover">
+                        <div class="fs_popover_item">
+                            <label class="fs_popover_title">{{ $t('Sort By') }}</label>
+                            <div class="fs_radio_blocks">
+                                <el-radio-group @change="fetchTickets" v-model="sorting.sort_by">
+                                    <el-radio v-for="column in sortingColumns" :key="column.value" :label="column.value">
+                                        {{ column.label }}
+                                    </el-radio>
+                                </el-radio-group>
+                            </div>
+                            <hr/>
+                            <el-radio-group class="fs_switch_button" size="small" v-model="sorting.sort_type">
+                                <el-radio-button label="asc">{{ $t('Ascending') }}</el-radio-button>
+                                <el-radio-button label="desc">{{ $t('Descending') }}</el-radio-button>
                             </el-radio-group>
-                        </div>
-                        <hr/>
-                        <el-radio-group class="fs_switch_button" size="small" v-model="sorting.sort_type">
-                            <el-radio-button label="asc">{{ $t('Ascending') }}</el-radio-button>
-                            <el-radio-button label="desc">{{ $t('Descending') }}</el-radio-button>
-                        </el-radio-group>
-                        <div class="fs_sorting_button">
-                            <el-button @click="fetchTickets()" type="primary">
-                                {{ $t('Apply') }}
-                            </el-button>
-                        </div>
-                    </div>
-                    <template #reference>
-                        <span>
-                            <el-tooltip effect="dark" :content="$t('Sort')" placement="top">
-                                <el-button>
-                                    <svg width="20" height="20" viewBox="0 0 20 20"
-                                         fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M16 4V13H18.25L15.25 16.75L12.25 13H14.5V4H16ZM10 14.5V16H3.25V14.5H10ZM11.5 9.25V10.75H3.25V9.25H11.5ZM11.5 4V5.5H3.25V4H11.5Z"
-                                            fill="#99A0AE"
-                                        />
-                                    </svg>
+                            <div class="fs_sorting_button">
+                                <el-button @click="fetchTickets()" type="primary">
+                                    {{ $t('Apply') }}
                                 </el-button>
-                            </el-tooltip>
-                        </span>
-                    </template>
-                </el-popover>
+                            </div>
+                        </div>
+                        <template #reference>
+                            <span>
+                                <el-tooltip effect="dark" :content="$t('Sort')" placement="top">
+                                    <el-button>
+                                        <svg width="20" height="20" viewBox="0 0 20 20"
+                                            fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M16 4V13H18.25L15.25 16.75L12.25 13H14.5V4H16ZM10 14.5V16H3.25V14.5H10ZM11.5 9.25V10.75H3.25V9.25H11.5ZM11.5 4V5.5H3.25V4H11.5Z"
+                                                fill="#99A0AE"
+                                            />
+                                        </svg>
+                                    </el-button>
+                                </el-tooltip>
+                            </span>
+                        </template>
+                    </el-popover>
+                </div>
             </div>
         </div>
 
