567f4a50956aa5fea933aaa1f5ce12ac7acb2bd4
bijit027 <debbijit70@gmail.com>
2025-03-18 11:14:54 +0600

    Fixed: Conflict in the div class name in the block editor

--- resources/block-editor/blocks/customer-portal/CreateTicket/LandingPage.jsx
+++ resources/block-editor/blocks/customer-portal/CreateTicket/LandingPage.jsx
@@ -23,8 +23,8 @@ export const CreateTicketBlock = props => {
                 setAttributes={setAttributes}
             />
 
-            <div className="fs_back_nav">
-                <button className="fs_back_button" onClick={() => showSection('allTickets')}>
+            <div className="fs_block_back_nav">
+                <button className="fs_block_back_button" onClick={() => showSection('allTickets')}>
                     <svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M2.20437 4.9992L5.91687 8.7117L4.85637 9.7722L0.083374 4.9992L4.85637 0.226196L5.91687 1.2867L2.20437 4.9992Z"
@@ -35,76 +35,76 @@ export const CreateTicketBlock = props => {
                 </button>
             </div>
 
-            <div className="fs_create_ticket" style={blockStyles}>
-                <div className="fs_create_ticket_header">
-                    <h2 className="fs_form_title">Submit a Support Ticket</h2>
+            <div className="fs_block_create_ticket" style={blockStyles}>
+                <div className="fs_block_create_ticket_header">
+                    <h2 className="fs_block_form_title">Submit a Support Ticket</h2>
                 </div>
 
-                <div className="fs_form_container">
-                    <div className="fs_form_group">
-                        <label className="fs_form_label">Subject</label>
+                <div className="fs_block_form_container">
+                    <div className="fs_block_form_group">
+                        <label className="fs_block_form_label">Subject</label>
                         <input
                             type="text"
-                            className="fs_form_input"
+                            className="fs_block_form_input"
                             placeholder="What's this support ticket about?"
 
                             style={inputFieldStyle}
                         />
                     </div>
 
-                    <div className="fs_form_group">
-                        <label className="fs_form_label">Ticket Details</label>
-                        <div className="fs_ticket_details_container">
-                            <div className="fs_ticket_details_tabs">
-                                <div className="fs_tabs_wrapper">
+                    <div className="fs_block_form_group">
+                        <label className="fs_block_form_label">Ticket Details</label>
+                        <div className="fs_block_ticket_details_container">
+                            <div className="fs_block_ticket_details_tabs">
+                                <div className="fs_block_tabs_wrapper">
                                     <button
-                                        className={`fs_tab_button ${activeTab === 'Visual' ? 'active' : ''}`}
+                                        className={`fs_block_tab_button ${activeTab === 'Visual' ? 'active' : ''}`}
                                         onClick={() => setActiveTab('Visual')}
                                     >
                                         Visual
                                     </button>
                                     <button
-                                        className={`fs_tab_button ${activeTab === 'Text' ? 'active' : ''}`}
+                                        className={`fs_block_tab_button ${activeTab === 'Text' ? 'active' : ''}`}
                                         onClick={() => setActiveTab('Text')}
                                     >
                                         Text
                                     </button>
                                 </div>
                             </div>
-                            <div className="fs_ticket_details_editor">
-                                <div className="fs_editor_toolbar">
-                                    <select className="fs_paragraph_dropdown">
+                            <div className="fs_block_ticket_details_editor">
+                                <div className="fs_block_editor_toolbar">
+                                    <select className="fs_block_paragraph_dropdown">
                                         <option>Paragraph</option>
                                     </select>
-                                    <button className="fs_toolbar_button fs_bold"><span
+                                    <button className="fs_block_toolbar_button fs_block_bold"><span
                                         className="dashicons dashicons-editor-bold"></span></button>
-                                    <button className="fs_toolbar_button fs_italic"><span
+                                    <button className="fs_block_toolbar_button fs_block_italic"><span
                                         className="dashicons dashicons-editor-italic"></span></button>
-                                    <button className="fs_toolbar_button fs_list_bullets"><span
+                                    <button className="fs_block_toolbar_button fs_block_list_bullets"><span
                                         className="dashicons dashicons-editor-ul"></span></button>
-                                    <button className="fs_toolbar_button fs_list_numbers"><span
+                                    <button className="fs_block_toolbar_button fs_block_list_numbers"><span
                                         className="dashicons dashicons-editor-ol"></span></button>
-                                    <button className="fs_toolbar_button fs_link"><span
+                                    <button className="fs_block_toolbar_button fs_block_link"><span
                                         className="dashicons dashicons-admin-links"></span></button>
-                                    <button className="fs_toolbar_button fs_quote"><span
+                                    <button className="fs_block_toolbar_button fs_block_quote"><span
                                         className="dashicons dashicons-editor-quote"></span></button>
-                                    <button className="fs_toolbar_button fs_align_left"><span
+                                    <button className="fs_block_toolbar_button fs_block_align_left"><span
                                         className="dashicons dashicons-editor-alignleft"></span></button>
-                                    <button className="fs_toolbar_button fs_align_center"><span
+                                    <button className="fs_block_toolbar_button fs_block_align_center"><span
                                         className="dashicons dashicons-editor-aligncenter"></span></button>
-                                    <button className="fs_toolbar_button fs_align_right"><span
+                                    <button className="fs_block_toolbar_button fs_block_align_right"><span
                                         className="dashicons dashicons-editor-alignright"></span></button>
-                                    <button className="fs_toolbar_button fs_align_justify"><span
+                                    <button className="fs_block_toolbar_button fs_block_align_justify"><span
                                         className="dashicons dashicons-editor-justify"></span></button>
-                                    <button className="fs_toolbar_button fs_font"><span
+                                    <button className="fs_block_toolbar_button fs_block_font"><span
                                         className="dashicons dashicons-editor-textcolor"></span></button>
-                                    <button className="fs_toolbar_button fs_undo"><span
+                                    <button className="fs_block_toolbar_button fs_block_undo"><span
                                         className="dashicons dashicons-undo"></span></button>
-                                    <button className="fs_toolbar_button fs_redo"><span
+                                    <button className="fs_block_toolbar_button fs_block_redo"><span
                                         className="dashicons dashicons-redo"></span></button>
                                 </div>
                                 <textarea
-                                    className="fs_editor_textarea"
+                                    className="fs_block_editor_textarea"
                                     placeholder="Enter ticket details here..."
                                     onChange={(e) => setDetails(e.target.value)}
                                 ></textarea>
@@ -112,20 +112,20 @@ export const CreateTicketBlock = props => {
                         </div>
                     </div>
 
-                    <div className="fs_form_group">
-                        <p className="fs_form_label">File Upload</p>
-                        <button className="fs_browse_file_button" style={secondaryButtonStyles}>Browse File</button>
-                        <div className="fs_upload_text_container">
-                            <p className="fs_upload_subtext">(Supported Types: Photos, CSV, PDF/Docs, Zip, JSON and max
+                    <div className="fs_block_form_group">
+                        <p className="fs_block_form_label">File Upload</p>
+                        <button className="fs_block_browse_file_button" style={secondaryButtonStyles}>Browse File</button>
+                        <div className="fs_block_upload_text_container">
+                            <p className="fs_block_upload_subtext">(Supported Types: Photos, CSV, PDF/Docs, Zip, JSON and max
                                 file size: 2.0MB)</p>
                         </div>
                     </div>
 
-                    <div className="fs_form_group">
-                        <label className="fs_form_label">Priority</label>
-                        <div className="fs_dropdown_container">
+                    <div className="fs_block_form_group">
+                        <label className="fs_block_form_label">Priority</label>
+                        <div className="fs_block_dropdown_container">
                             <select
-                                className="fs_form_dropdown"
+                                className="fs_block_form_dropdown"
                                 value={priority}
                                 style={inputFieldStyle}
                                 onChange={(e) => setPriority(e.target.value)}
@@ -138,19 +138,19 @@ export const CreateTicketBlock = props => {
                         </div>
                     </div>
 
-                    <div className="fs_form_group">
-                        <label className="fs_form_label">Email Address</label>
+                    <div className="fs_block_form_group">
+                        <label className="fs_block_form_label">Email Address</label>
                         <input
                             type="text"
-                            className= "fs_form_input"
+                            className= "fs_block_form_input"
                             placeholder="Enter your email"
                             style={inputFieldStyle}
                         />
                     </div>
 
-                    <div className="fs_form_actions">
+                    <div className="fs_block_form_actions">
                         <button
-                            className="fs_create_ticket_submit_button"
+                            className="fs_block_create_ticket_submit_button"
                             style={primaryButtonStyles}
                         >
                             Create Ticket
--- resources/block-editor/blocks/customer-portal/ViewTicket/LandingPage.jsx
+++ resources/block-editor/blocks/customer-portal/ViewTicket/LandingPage.jsx
@@ -41,149 +41,149 @@ export const ViewTicketBlock = props => {
                 </button>
             </div>
 
-            <div className="fs_editor_view_ticket" style={blockStyles}>
-                <div className="fs_editor_view_ticket_header">
-                    <h2 className="fs_ticket_title">#2053 Premium Access Not Working</h2>
-                    <div className="fs_ticket_status active">Active</div>
+            <div className="fs_block_editor_view_ticket" style={blockStyles}>
+                <div className="fs_block_editor_view_ticket_header">
+                    <h2 className="fs_block_ticket_title">#2053 Premium Access Not Working</h2>
+                    <div className="fs_block_ticket_status active">Active</div>
                 </div>
 
-                <div className="fs_ticket_actions">
-                    <button className="fs_refresh_button" style={secondaryButtonStyles}>
+                <div className="fs_block_ticket_actions">
+                    <button className="fs_block_refresh_button" style={secondaryButtonStyles}>
                         <span className="dashicons dashicons-update-alt"></span> Refresh
                     </button>
-                    <button className="fs_close_ticket_button" style={secondaryButtonStyles}>
+                    <button className="fs_block_close_ticket_button" style={secondaryButtonStyles}>
                         Close Ticket
                     </button>
                 </div>
 
-                <div className="fs_ticket_privacy_notice">
-                    <span className="fs_privacy_icon">i</span>
-                    <p className="fs_privacy_text">This ticket is private—only you and official support agents can view
+                <div className="fs_block_ticket_privacy_notice">
+                    <span className="fs_block_privacy_icon">i</span>
+                    <p className="fs_block_privacy_text">This ticket is private—only you and official support agents can view
                         it.</p>
-                    <button className="fs_close_notice_button">×</button>
+                    <button className="fs_block_close_notice_button">×</button>
                 </div>
 
-                <div className="fs_reply_container">
-                    <label className="fs_reply_header">Write a reply</label>
-                    <div className="fs_ticket_details_container">
-                        <div className="fs_ticket_details_tabs">
-                            <div className="fs_tabs_wrapper">
+                <div className="fs_block_reply_container">
+                    <label className="fs_block_reply_header">Write a reply</label>
+                    <div className="fs_block_ticket_details_container">
+                        <div className="fs_block_ticket_details_tabs">
+                            <div className="fs_block_tabs_wrapper">
                                 <button
-                                    className={`fs_tab_button ${activeTab === 'Visual' ? 'active' : ''}`}
+                                    className={`fs_block_tab_button ${activeTab === 'Visual' ? 'active' : ''}`}
                                     onClick={() => setActiveTab('Visual')}
                                 >
                                     Visual
                                 </button>
                                 <button
-                                    className={`fs_tab_button ${activeTab === 'Text' ? 'active' : ''}`}
+                                    className={`fs_block_tab_button ${activeTab === 'Text' ? 'active' : ''}`}
                                     onClick={() => setActiveTab('Text')}
                                 >
                                     Text
                                 </button>
                             </div>
                         </div>
-                        <div className="fs_ticket_details_editor">
-                            <div className="fs_editor_toolbar">
-                                <select className="fs_paragraph_dropdown">
+                        <div className="fs_block_ticket_details_editor">
+                            <div className="fs_block_editor_toolbar">
+                                <select className="fs_block_paragraph_dropdown">
                                     <option>Paragraph</option>
                                 </select>
-                                <button className="fs_toolbar_button fs_bold"><span
+                                <button className="fs_block_toolbar_button fs_block_bold"><span
                                     className="dashicons dashicons-editor-bold"></span></button>
-                                <button className="fs_toolbar_button fs_italic"><span
+                                <button className="fs_block_toolbar_button fs_block_italic"><span
                                     className="dashicons dashicons-editor-italic"></span></button>
-                                <button className="fs_toolbar_button fs_list_bullets"><span
+                                <button className="fs_block_toolbar_button fs_block_list_bullets"><span
                                     className="dashicons dashicons-editor-ul"></span></button>
-                                <button className="fs_toolbar_button fs_list_numbers"><span
+                                <button className="fs_block_toolbar_button fs_block_list_numbers"><span
                                     className="dashicons dashicons-editor-ol"></span></button>
-                                <button className="fs_toolbar_button fs_link"><span
+                                <button className="fs_block_toolbar_button fs_block_link"><span
                                     className="dashicons dashicons-admin-links"></span></button>
-                                <button className="fs_toolbar_button fs_quote"><span
+                                <button className="fs_block_toolbar_button fs_block_quote"><span
                                     className="dashicons dashicons-editor-quote"></span></button>
-                                <button className="fs_toolbar_button fs_align_left"><span
+                                <button className="fs_block_toolbar_button fs_block_align_left"><span
                                     className="dashicons dashicons-editor-alignleft"></span></button>
-                                <button className="fs_toolbar_button fs_align_center"><span
+                                <button className="fs_block_toolbar_button fs_block_align_center"><span
                                     className="dashicons dashicons-editor-aligncenter"></span></button>
-                                <button className="fs_toolbar_button fs_align_right"><span
+                                <button className="fs_block_toolbar_button fs_block_align_right"><span
                                     className="dashicons dashicons-editor-alignright"></span></button>
-                                <button className="fs_toolbar_button fs_align_justify"><span
+                                <button className="fs_block_toolbar_button fs_block_align_justify"><span
                                     className="dashicons dashicons-editor-justify"></span></button>
-                                <button className="fs_toolbar_button fs_font"><span
+                                <button className="fs_block_toolbar_button fs_block_font"><span
                                     className="dashicons dashicons-editor-textcolor"></span></button>
-                                <button className="fs_toolbar_button fs_undo"><span
+                                <button className="fs_block_toolbar_button fs_block_undo"><span
                                     className="dashicons dashicons-undo"></span></button>
-                                <button className="fs_toolbar_button fs_redo"><span
+                                <button className="fs_block_toolbar_button fs_block_redo"><span
                                     className="dashicons dashicons-redo"></span></button>
                             </div>
                             <textarea
-                                className="fs_editor_textarea"
+                                className="fs_block_editor_textarea"
                                 placeholder="Enter ticket details here..."
                                 value={details}
                                 onChange={(e) => setDetails(e.target.value)}
                             ></textarea>
                         </div>
                     </div>
-                    <div className="fs_attachment_section">
-                        <h4 className="fs_attachment_header">Add Attachment</h4>
-                        <button className="fs_browse_file_button" style={secondaryButtonStyles}>
+                    <div className="fs_block_attachment_section">
+                        <h4 className="fs_block_attachment_header">Add Attachment</h4>
+                        <button className="fs_block_browse_file_button" style={secondaryButtonStyles}>
                             <span className="dashicons dashicons-upload"></span> Browse File
                         </button>
-                        <p className="fs_attachment_info">
+                        <p className="fs_block_attachment_info">
                             (Supported Types: Photos, CSV, PDF/Docs, Zip, JSON and max file size: 2.0MB)
                         </p>
                     </div>
 
-                    <div className="fs_reply_actions">
-                        <a href="#" className="fs_reply_and_close_link">
+                    <div className="fs_block_reply_actions">
+                        <a href="#" className="fs_block_reply_and_close_link">
                             Reply and Close
                         </a>
                         <button
-                            className="fs_reply_button"
+                            className="fs_block_reply_button"
                             style={primaryButtonStyles}
                         >
                             Reply <span className="dashicons dashicons-arrow-right-alt"></span>
                         </button>
                     </div>
                 </div>
-                <div className="fs_conversation_container">
-                    <div className="fs_conversation_message fs_agent_message">
-                        <div className="fs_message_avatar" style={avatarStyle}>
+                <div className="fs_block_conversation_container">
+                    <div className="fs_block_conversation_message fs_block_agent_message">
+                        <div className="fs_block_message_avatar" style={avatarStyle}>
                             <img src="https://secure.gravatar.com/avatar/?s=96&amp;d=mm&amp;r=g"
                                  alt="Support Agent Avatar"/>
                         </div>
-                        <div className="fs_message_content">
-                            <div className="fs_message_header">
-                                <div className="fs_message_author">
-                                    <span className="fs_author_name">Support Agent</span>
-                                    <span className="fs_author_role">Support Team</span>
+                        <div className="fs_block_message_content">
+                            <div className="fs_block_message_header">
+                                <div className="fs_block_message_author">
+                                    <span className="fs_block_author_name">Support Agent</span>
+                                    <span className="fs_block_author_role">Support Team</span>
                                 </div>
                             </div>
-                            <div className="fs_message_text">
+                            <div className="fs_block_message_text">
                                 <p>Hi! Thanks for reaching out. I’ve checked your account, and there was a delay in
                                     syncing your subscription. I’ve manually updated it, and you should now have full
                                     access. Please refresh your page and let me know if everything works!</p>
                             </div>
-                            <div className="fs_message_footer">
-                                <span className="fs_message_timestamp">1 day ago</span>
+                            <div className="fs_block_message_footer">
+                                <span className="fs_block_message_timestamp">1 day ago</span>
                             </div>
                         </div>
                     </div>
-                    <div className="fs_conversation_message fs_thread_start">
-                        <div className="fs_message_avatar" style={avatarStyle}>
+                    <div className="fs_block_conversation_message fs_block_thread_start">
+                        <div className="fs_block_message_avatar" style={avatarStyle}>
                             <img src="https://secure.gravatar.com/avatar/?s=96&amp;d=mm&amp;r=g" alt="User Avatar"/>
                         </div>
-                        <div className="fs_message_content">
-                            <div className="fs_message_header">
-                                <div className="fs_message_author">
-                                    <span className="fs_author_name">You</span>
-                                    <span className="fs_author_info">started the conversation</span>
+                        <div className="fs_block_message_content">
+                            <div className="fs_block_message_header">
+                                <div className="fs_block_message_author">
+                                    <span className="fs_block_author_name">You</span>
+                                    <span className="fs_block_author_info">started the conversation</span>
                                 </div>
                             </div>
-                            <div className="fs_message_text">
+                            <div className="fs_block_message_text">
                                 <p>Hi, I upgraded to the premium plan, but my account still shows the free plan
                                     restrictions. Can you check?</p>
                             </div>
-                            <div className="fs_message_footer">
-                                <span className="fs_message_timestamp">2 days ago</span>
+                            <div className="fs_block_message_footer">
+                                <span className="fs_block_message_timestamp">2 days ago</span>
                             </div>
                         </div>
                     </div>
