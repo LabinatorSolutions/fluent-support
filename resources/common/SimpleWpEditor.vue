9eba4ef56b04b917cbccda6b2afc1ba88f682018
bijit027 <debbijit70@gmail.com>
2025-04-17 12:17:39 +0600

    Fixed: Issues in the plain text editor.

--- resources/common/SimpleWpEditor.vue
+++ resources/common/SimpleWpEditor.vue
@@ -58,8 +58,8 @@ export default {
     },
     data() {
         return {
-            hasWpEditor: (!!window.wp.editor && !!wp.editor.autop) || !!window.wp.oldEditor,
-            editor: window.wp.oldEditor || window.wp.editor,
+            hasWpEditor: (!!window.wp?.editor && !!wp?.editor?.autop) || !!window.wp?.oldEditor,
+            editor: window.wp?.oldEditor || window.wp?.editor,
             plain_content: this.modelValue,
             cursorPos: (this.modelValue) ? this.modelValue.length : 0,
             app_ready: false,
